eeb5fdf069663d2c28eb936babc9be03f4864434
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-08 13:42:51 -0400

    Update RestBuilder.js

--- RestBuilder.js
+++ RestBuilder.js
@@ -4,3 +4,11 @@ function openRestBuilder(orgUrl) {
   var windowOptions = "height=600,width=800,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,titlebar=no,toolbar=no";
   window.open(restBuilderUrl, "REST Builder", windowOptions);
 }
+function getOrgUrl() {
+  if (typeof Xrm !== 'undefined' && Xrm.Page && Xrm.Page.context) {
+    return Xrm.Page.context.getClientUrl();
+  } else {
+    console.error('Unable to retrieve organization URL. Pleaes run this within a Dynamics CRM environment.');
+    return '';
+  }
+}
