2eec4483a8f064458c79c2af61d68a18fdf7492c
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-08 13:28:31 -0400

    Add files via upload

---
+++ showHiddenItems_UnlockFields.js
@@ -0,0 +1,40 @@
+function unlockAllFields() {
+    var currentFormId = Xrm.Page.ui.formSelector.getCurrentItem().getId();
+    if (lastUpdatedFormId === currentFormId && unlockAllFieldsBtnClickStatus) {        
+        return;
+    }
+	var allControls = Xrm.Page.ui.controls.get();
+	for (var i in allControls) {
+		var control = allControls[i];
+		if (control) {
+			control.setDisabled(false);
+		}
+	}
+    unlockAllFieldsBtnClickStatus = true;
+    lastUpdatedFormId = currentFormId;
+}
+
+function showAllTabsAndSections() {
+    var currentFormId = Xrm.Page.ui.formSelector.getCurrentItem().getId();
+    if (lastUpdatedFormId === currentFormId && showAllTabsAndSectionsBtnClickStatus) {        
+        return;
+    }
+	Xrm.Page.ui.tabs.forEach(function(tab) {
+		if (!tab.getVisible()) {
+			tab.setVisible(true);			
+		}
+		tab.sections.forEach(function(section) {
+			if (!section.getVisible()) {
+				section.setVisible(true);
+			}
+			section.controls.forEach(function(control) {
+				if (!control.getVisible()) {
+					control.setVisible(true);
+				}
+			});
+		});
+	});
+    showAllTabsAndSectionsBtnClickStatus = true;
+    lastUpdatedFormId = currentFormId;
+    
+}
