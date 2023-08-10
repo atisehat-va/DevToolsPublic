93d851e7a494f0f6f7d6b028fe91d9f87e21e711
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-10 18:10:02 -0400

    Update main-popup.js

--- main-popup.js
+++ main-popup.js
@@ -179,6 +179,12 @@ function makePopupMovable(popupDiv) {
 
   function dragMouseDown(e) {
     e = e || window.event;
+    //new
+    var iframeContainer = document.getElementById('iframe-container');
+    if (iframeContainer.contains(e.target)) {
+	return;
+    }
+    //newEnd
     e.preventDefault();
     pos3 = e.clientX;
     pos4 = e.clientY;
