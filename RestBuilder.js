bc7b7056fc7a9bd72cb2722d62ff11e682c1666a
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-10 15:02:27 -0400

    Update RestBuilder.js

--- RestBuilder.js
+++ RestBuilder.js
@@ -1,4 +1,5 @@
 function openRestBuilder(orgUrl) {
+  closeIframe();
   var restBuilderPath = '/WebResources/lat_/CRMRESTBuilder/Xrm.RESTBuilder.htm#';
   var restBuilderUrl = orgUrl + restBuilderPath;
   var windowOptions = "height=600,width=800,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,titlebar=no,toolbar=no";
