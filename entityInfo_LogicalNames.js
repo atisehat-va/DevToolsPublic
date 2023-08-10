cac03986077ed2eb30c0f2862b2c3d0351af501a
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-10 15:27:28 -0400

    Update entityInfo_LogicalNames.js

--- entityInfo_LogicalNames.js
+++ entityInfo_LogicalNames.js
@@ -1,4 +1,4 @@
-function fetchEntityFields() {
+/*function fetchEntityFields() {
 	closeIframe();
 	var entityName = Xrm.Page.data.entity.getEntityName();
 	var entityId = Xrm.Page.data.entity.getId();
@@ -21,6 +21,35 @@ function fetchEntityFields() {
 				var newWindow = window.open();
 				newWindow.document.write(html);
 				newWindow.document.close();
+			} else {
+				alert("Error: " + this.statusText);
+			}
+		}
+	};
+	xhr.send();
+}*/
+function fetchEntityFields() {
+	closeIframe();
+	var entityName = Xrm.Page.data.entity.getEntityName();
+	var entityId = Xrm.Page.data.entity.getId();
+	var url = Xrm.Page.context.getClientUrl() + "/api/data/v9.1/EntityDefinitions(LogicalName='" + entityName + "')/Attributes?$select=LogicalName,AttributeType";
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", url);
+	xhr.onreadystatechange = function() {
+		if (this.readyState === 4) {
+			if (this.status === 200) {
+				var results = JSON.parse(this.responseText);
+				var fieldList = results.value
+					.filter(function(field) {
+						return field.AttributeType !== 'Virtual';
+					})
+					.map(function(field, index) {
+						return '<li>' + (index + 1) + '. ' + field.LogicalName + ' (' + field.AttributeType + ')</li>';
+					})
+					.join('');
+				        var html = '<h2>Entity: ' + entityName + '</h2><h2>Record ID: ' + entityId + '</h2><h2>Fields:</h2><ul style="columns: 4; -webkit-columns: 4; -moz-columns: 4;">' + fieldList + '</ul>';
+				var newWindow = window.open();
+				showContent('alert', html);
 			} else {
 				alert("Error: " + this.statusText);
 			}
