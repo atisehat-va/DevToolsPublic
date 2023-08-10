6192bae1e971871d134a66340a7972ca9629b668
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-10 15:47:55 -0400

    Update main-popup.js

--- main-popup.js
+++ main-popup.js
@@ -42,6 +42,7 @@ function openPopup() {
         .container { display: flex; flex-direction: row; width: 400px; transition: width 0.5s; }
         .container.expanded-iframe { width: 900px; height: 500px; }
 	.container.expanded-alert { width: 900px; height: 248px;}
+        .container.expanded-html { width: 900px; height: 500px;}
         .button-container { width: 400px; }
         .iframe-container { display: none; flex-grow: 1; position: relative; padding: 20px; }
         .popup button { display: block; width: 100%; margin-bottom: 10px; padding: 10px; background-color: #002050; color: white; border: none; }
@@ -146,7 +147,7 @@ function showContent(contentType, url) {
       htmlDiv.innerHTML = url;
       contentDiv.appendChild(htmlDiv);      
       iframeContainer.style.display = 'block';
-      containerDiv.classList.add('expanded-alert');
+      containerDiv.classList.add('expanded-html');
       break;
     default:
       console.error('Invalid content type');
