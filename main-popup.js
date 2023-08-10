6b468bda72a4ebc74cd04d6be8cf93cce3facf47
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-10 15:37:33 -0400

    Update main-popup.js

--- main-popup.js
+++ main-popup.js
@@ -55,6 +55,7 @@ function openPopup() {
         .dropdown-content button { display: block; background-color: white; color: black; padding: 10px; text-align: left; border: none; width: 100%; }
         .content { display: none; width: 100%; border-top: 1px solid #888; padding-top: 10px; }                
 	.alert-message { text-align: center; boarder-radius: 5px; background-color: #fee; padding: 10px; margin: 10px font-weight: bold; color: #900; }
+ 	.html { boarder-radius: 5px; background-color: #fee; padding: 10px; margin: 10px font-weight: bold; color: #900; }
      </style>
     <div class="popup">
 		<div class="container" id="container">
@@ -141,7 +142,7 @@ function showContent(contentType, url) {
       break;
     case 'html':
       var htmlDiv = document.createElement('div');      
-      htmlDiv.className = 'alert-message';      
+      htmlDiv.className = 'html';      
       htmlDiv.innerHTML = url;
       contentDiv.appendChild(htmlDiv);      
       iframeContainer.style.display = 'block';
