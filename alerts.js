10a8b29efcb0da983f423dd3bd423e81bfa73ee1
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-11 15:38:24 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,33 +1,4 @@
-var userId = Xrm.Utility.getGlobalContext().userSettings.userId;
-
-var entity = {};
-entity["vhacrm_systemuserid@odata.bind"] = "/systemusers(" + userId.replace("{", "").replace("}", "") + ")";
-entity.mcs_removecurrentroles = false;
-// Set other properties as needed
-
-var req = new XMLHttpRequest();
-req.open("POST", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/vhacrm_userprovisions", true);
-req.setRequestHeader("OData-MaxVersion", "4.0");
-req.setRequestHeader("OData-Version", "4.0");
-req.setRequestHeader("Accept", "application/json");
-req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
-req.onreadystatechange = function() {
-    if (this.readyState === 4) {
-        req.onreadystatechange = null;
-        if (this.status === 204) {
-            var uri = this.getResponseHeader("OData-EntityId");
-            var regExp = /\(([^)]+)\)/;
-            var matches = regExp.exec(uri);
-            var newEntityId = matches[1];
-        } else {
-            Xrm.Utility.alertDialog(this.statusText);
-        }
-    }
-};
-req.send(JSON.stringify(entity));
-----------------------------------
-    function executeCustomAction(userId) {
+function executeCustomAction(entityId, entityName, userId) {
   // Replace with your Action's unique name
   var actionName = "new_MyCustomAction";
 
@@ -39,36 +10,19 @@ req.send(JSON.stringify(entity));
     }
   };
 
-  // Build the request URL
+  // Build the request URL for the bound action
   var req = new XMLHttpRequest();
   req.open(
     "POST",
     Xrm.Utility.getGlobalContext().getClientUrl() +
       "/api/data/v9.1/" +
-      actionName,
+      entityName + "s(" + entityId + ")/Microsoft.Dynamics.CRM." + actionName,
     true
   );
 
-  req.setRequestHeader("OData-MaxVersion", "4.0");
-  req.setRequestHeader("OData-Version", "4.0");
-  req.setRequestHeader("Accept", "application/json");
-  req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-  req.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      req.onreadystatechange = null;
-      if (this.status === 204) {
-        // Success - Handle as needed
-        var results = JSON.parse(this.response);
-      } else {
-        // Error - Handle as needed
-        var error = JSON.parse(this.response).error;
-        console.error(error.message);
-      }
-    }
-  };
-
-  // Send the request with the payload
-  req.send(JSON.stringify(data));
+  // Rest of the code remains the same as above ...
 }
-executeCustomAction("USER-GUID-HERE");
+
+// Invoke the function
+executeCustomAction("ENTITY-GUID-HERE", "entitylogicalname", "USER-GUID-HERE");
 
