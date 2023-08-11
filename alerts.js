3c7e316cd6c52e845ac43e9da2c67505e603faf2
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-11 15:30:16 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -26,3 +26,49 @@ req.onreadystatechange = function() {
     }
 };
 req.send(JSON.stringify(entity));
+----------------------------------
+    function executeCustomAction(userId) {
+  // Replace with your Action's unique name
+  var actionName = "new_MyCustomAction";
+
+  // Define the request payload
+  var data = {
+    UserName: {
+      id: userId, // GUID of the User entity
+      entityType: "systemuser" // Entity logical name
+    }
+  };
+
+  // Build the request URL
+  var req = new XMLHttpRequest();
+  req.open(
+    "POST",
+    Xrm.Utility.getGlobalContext().getClientUrl() +
+      "/api/data/v9.1/" +
+      actionName,
+    true
+  );
+
+  req.setRequestHeader("OData-MaxVersion", "4.0");
+  req.setRequestHeader("OData-Version", "4.0");
+  req.setRequestHeader("Accept", "application/json");
+  req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+  req.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      req.onreadystatechange = null;
+      if (this.status === 204) {
+        // Success - Handle as needed
+        var results = JSON.parse(this.response);
+      } else {
+        // Error - Handle as needed
+        var error = JSON.parse(this.response).error;
+        console.error(error.message);
+      }
+    }
+  };
+
+  // Send the request with the payload
+  req.send(JSON.stringify(data));
+}
+executeCustomAction("USER-GUID-HERE");
+
