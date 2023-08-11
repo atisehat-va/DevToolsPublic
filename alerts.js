b75e4db009bfbf24ee907412403d87a8ac44564f
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-11 16:24:42 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,28 +1,26 @@
-function executeCustomAction(entityId, entityName, userId) {
-  // Replace with your Action's unique name
-  var actionName = "new_MyCustomAction";
+var userIdToImpersonate = "AE2280C2-84B8-ED11-83FE-001DD8070A7E"; // The ID of the user you want to impersonate
+var entityToUpdateId = "B32280D2-95B8-ED11-83FE-001DD8070B7C"; // The ID of the entity you want to update
+var entityName = "vhacrm_userprovisions"; // The logical name of the entity you want to update
 
-  // Define the request payload
-  var data = {
-    UserName: {
-      id: userId, // GUID of the User entity
-      entityType: "systemuser" // Entity logical name
+var entity = {};
+entity.mcs_removecurrentroles = false; // Set any properties you want to update
+
+var req = new XMLHttpRequest();
+req.open("PATCH", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/" + entityName + "(" + entityToUpdateId + ")", true);
+req.setRequestHeader("OData-MaxVersion", "4.0");
+req.setRequestHeader("OData-Version", "4.0");
+req.setRequestHeader("Accept", "application/json");
+req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+req.setRequestHeader("MSCRMCallerID", userIdToImpersonate); // This header allows you to impersonate another user
+
+req.onreadystatechange = function() {
+    if (this.readyState === 4) {
+        req.onreadystatechange = null;
+        if (this.status === 204) {
+            Xrm.Utility.alertDialog("Entity updated successfully!");
+        } else {
+            Xrm.Utility.alertDialog(this.statusText);
+        }
     }
-  };
-
-  // Build the request URL for the bound action
-  var req = new XMLHttpRequest();
-  req.open(
-    "POST",
-    Xrm.Utility.getGlobalContext().getClientUrl() +
-      "/api/data/v9.1/" +
-      entityName + "s(" + entityId + ")/Microsoft.Dynamics.CRM." + actionName,
-    true
-  );
-
-  // Rest of the code remains the same as above ...
-}
-
-// Invoke the function
-executeCustomAction("ENTITY-GUID-HERE", "entitylogicalname", "USER-GUID-HERE");
-
+};
+req.send(JSON.stringify(entity));
