7524ea8d426cdfc63397f7333f28a012df5eca66
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-11 13:35:55 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,4 +1,7 @@
 var entity = {};
+entity["vhacrm_systemuserid@odata.bind"] = "/systemusers(AE2280C2-84B8-ED11-83FE-001DD8070A7E)";
+entity.mcs_removecurrentroles = false;
+entity["vhacrm_userprovisionroleid@odata.bind"] = "/vhacrm_userprovisionroles(AE2280C2-84B8-ED11-83FE-001DD8070A7E)";
 
 var req = new XMLHttpRequest();
 req.open("POST", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/vhacrm_userprovisions", true);
@@ -7,7 +10,6 @@ req.setRequestHeader("OData-Version", "4.0");
 req.setRequestHeader("Accept", "application/json");
 req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
 req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
-req.setRequestHeader("MSCRMCallerID", "ae2280c2-84b8-ed11-83fe-001dd8070a7e");
 req.onreadystatechange = function() {
     if (this.readyState === 4) {
         req.onreadystatechange = null;
