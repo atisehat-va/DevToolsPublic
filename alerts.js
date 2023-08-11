0c8de9c41def8793d8a893fcc20dba655de10c4c
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-11 13:29:41 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,20 +1,21 @@
 var entity = {};
 
 var req = new XMLHttpRequest();
-var serverUrl = Xrm.Page.context.getClientUrl(); // Get the server URL from Dynamics 365 context
-req.open("POST", serverUrl + "/api/data/v9.0/tasks", true);
+req.open("POST", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/vhacrm_userprovisions", true);
 req.setRequestHeader("OData-MaxVersion", "4.0");
 req.setRequestHeader("OData-Version", "4.0");
 req.setRequestHeader("Accept", "application/json");
 req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
 req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
-req.setRequestHeader("MSCRMCallerID", "60FBEAFB-7724-EB11-A813-000D3A569CF5"); // Replace with the desired user ID
-
-req.onreadystatechange = function () {
+req.setRequestHeader("MSCRMCallerID", "ae2280c2-84b8-ed11-83fe-001dd8070a7e");
+req.onreadystatechange = function() {
     if (this.readyState === 4) {
         req.onreadystatechange = null;
         if (this.status === 204) {
-            Xrm.Utility.alertDialog("New Task Record Created");
+            var uri = this.getResponseHeader("OData-EntityId");
+            var regExp = /\(([^)]+)\)/;
+            var matches = regExp.exec(uri);
+            var newEntityId = matches[1];
         } else {
             Xrm.Utility.alertDialog(this.statusText);
         }
