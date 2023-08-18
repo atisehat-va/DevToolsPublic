dff1465b0a240d10fe4c6c0c92339506c4f294b7
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-18 12:11:45 -0400

    Update entityInfo_LogicalNames.js

--- entityInfo_LogicalNames.js
+++ entityInfo_LogicalNames.js
@@ -90,7 +90,7 @@ function updateOptionSetValues(control) {
 	var optionSetOptions = control.getOptions();
 	optionSetOptions.forEach(function(option) {
 		if (option.text !== "") {
-			var newText = option.value.toString() + " (" + option.text.split(" ")[0] + ")";
+			var newText = option.value.toString() + " (" + option.text + ")";
 			control.removeOption(option.value);
 			control.addOption({
 				value: option.value,
