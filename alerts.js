ab4c6f72c5deb7e4eff4c9e2a7ae8ac825d5ccde
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-18 12:14:12 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -2,7 +2,13 @@ function updateOptionSetValues(control) {
 	var optionSetOptions = control.getOptions();
 	optionSetOptions.forEach(function(option) {
 		if (option.text !== "") {
-			var newText = option.value.toString() + " (" + option.text + ")";
+			// Extract the original text by removing the value part if it exists
+			var originalText = option.text.split("(").pop().split(")")[0];
+			
+			// Combine the value and the original text
+			var newText = option.value.toString() + " (" + originalText + ")";
+			
+			// Update the option
 			control.removeOption(option.value);
 			control.addOption({
 				value: option.value,
