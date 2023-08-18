6913c8386ea6220215de197cfbab85be4c49261e
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-18 12:09:09 -0400

    Update entityInfo_LogicalNames.js

--- entityInfo_LogicalNames.js
+++ entityInfo_LogicalNames.js
@@ -60,7 +60,7 @@ function renameTabsSectionsFields() {
     logicalNameBtnClickStatus = true; 
     lastUpdatedFormId = currentFormId;
 }
-/*
+
 function renameControlAndUpdateOptionSet(control) {
 	var attribute = control.getAttribute();
 	if (attribute !== null) {
@@ -71,7 +71,7 @@ function renameControlAndUpdateOptionSet(control) {
 		}
 	}
 }
-
+/*
 function updateOptionSetValues(control) {    
 	var optionSetOptions = control.getOptions();        
 	optionSetOptions.forEach(function(option) {
@@ -85,40 +85,17 @@ function updateOptionSetValues(control) {
 	});   
 } */
 //testNew
-function renameControlAndUpdateOptionSet(control) {
-    var attribute = control.getAttribute();
-    if (attribute !== null) {
-        var logicalName = attribute.getName();
-        control.setLabel(logicalName);
-        if (control.getControlType() === "optionset") {
-            updateOptionSetValues(control);            
-        }
-    }
-}
 
 function updateOptionSetValues(control) {
-    var optionSetOptions = control.getOptions();
-    var updatedOptions = [];
-
-    // Create the updated options array without modifying the original options
-    optionSetOptions.forEach(function(option) {
-        if (option.text.indexOf("(") === -1) { // Check if the text has not been updated before
-            updatedOptions.push({
-                value: option.value,
-                text: option.value.toString() + " (" + option.text + ")"
-            });
-        } else {
-            updatedOptions.push(option); // Keep the option as is if it was already updated
-        }
-    });
-
-    // Clear the existing options
-    optionSetOptions.forEach(function(option) {
-        control.removeOption(option.value);
-    });
-
-    // Add the updated options
-    updatedOptions.forEach(function(option) {
-        control.addOption(option, option.value);
-    });
+	var optionSetOptions = control.getOptions();
+	optionSetOptions.forEach(function(option) {
+		if (option.text !== "") {
+			var newText = option.value.toString() + " (" + option.text.split(" ")[0] + ")";
+			control.removeOption(option.value);
+			control.addOption({
+				value: option.value,
+				text: newText
+			}, option.value);
+		}
+	});
 }
