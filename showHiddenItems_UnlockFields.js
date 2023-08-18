ac0f83f2f77f6f45b3596b3844f93fffc3a1a35d
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-18 17:21:23 -0400

    Update showHiddenItems_UnlockFields.js

--- showHiddenItems_UnlockFields.js
+++ showHiddenItems_UnlockFields.js
@@ -21,7 +21,7 @@ function showAllTabsAndSections() {
 	debugger;
     var currentFormId = Xrm.Page.ui.formSelector.getCurrentItem().getId();
     if (lastUpdatedFormId === currentFormId && showAllTabsAndSectionsBtnClickStatus) {
-	showContent('alert', 'Show Hidden Items button has already been clicked!!1');
+	showContent('alert', 'Show Hidden Items button has already been clicked!!');
         //return;
     }
 	Xrm.Page.ui.tabs.forEach(function(tab) {
