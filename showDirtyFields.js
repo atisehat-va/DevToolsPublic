4ed0cc6705720eaae7d94e56a3a806ada55acc96
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-18 15:34:38 -0400

    Update showDirtyFields.js

--- showDirtyFields.js
+++ showDirtyFields.js
@@ -65,7 +65,7 @@ function showDirtyFields() {
             var displayName = control ? control.getLabel() : logicalName;
 
             return '<div>' + (index + 1) + '. <strong>' + displayName + '</strong></div>' +
-                   '<div style="margin-left: 40px;">&bull; <strong>Logical Name:</strong> ' + logicalName + '</div>';
+                   '<div style="margin-left: 40px;">&bull; <strong>Logical Name:</strong> ' + logicalName + '</div>' '</br>';
         }).join('');
 
         html += '<div style="padding: 5px;">' + fieldList + '</div>';
