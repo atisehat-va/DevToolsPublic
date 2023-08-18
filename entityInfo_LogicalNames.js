df0915153af6a592989b4cb8f164d97dc34be87a
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-18 11:46:36 -0400

    Update entityInfo_LogicalNames.js

--- entityInfo_LogicalNames.js
+++ entityInfo_LogicalNames.js
@@ -60,7 +60,7 @@ function renameTabsSectionsFields() {
     logicalNameBtnClickStatus = true; 
     lastUpdatedFormId = currentFormId;
 }
-
+/*
 function renameControlAndUpdateOptionSet(control) {
 	var attribute = control.getAttribute();
 	if (attribute !== null) {
@@ -83,4 +83,39 @@ function updateOptionSetValues(control) {
            }, option.value); 
         }
 	});   
+} */
+//testNew
+function renameControlAndUpdateOptionSet(control) {
+    var attribute = control.getAttribute();
+    if (attribute !== null) {
+        var logicalName = attribute.getName();
+        control.setLabel(logicalName);
+        if (control.getControlType() === "optionset") {
+            updateOptionSetValues(control);            
+        }
+    }
+}
+function updateOptionSetValues(control) {    
+    var optionSetOptions = control.getOptions();        
+    var updatedOptions = [];
+    
+    // Iterate through the existing options and prepare the updated options
+    optionSetOptions.forEach(function(option) {
+        if (option.text !== "") {
+            updatedOptions.push({
+                value: option.value,
+                text: option.value.toString() + " (" + option.text + ")"
+            });
+        }
+    });
+
+    // Clear the existing options
+    optionSetOptions.forEach(function(option) {
+        control.removeOption(option.value);
+    });
+
+    // Add the updated options
+    updatedOptions.forEach(function(option) {
+        control.addOption(option, option.value);
+    });
 }
