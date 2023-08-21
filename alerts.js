3148c595e08addcacef1ccb0b066b793edc5b04a
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-21 17:48:14 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -9,14 +9,15 @@ javascript: (function() {
   }
 
   function displayPopup(users) {
+    users.entities.sort((a, b) => a.fullname.localeCompare(b.fullname));
     var popupHtml = `
       <div class="popup">
         <style>
           .popup { display: flex; flex-direction: column; align-items: flex-start; background-color: white; border: 1px solid #888; padding: 20px; width: 300px; }
-          #userList { max-height: 200px; overflow-y: scroll; height: 200px; }
-          #searchInput { width: 100%; }
+          #userList, #searchInput { width: 100%; }
+          #userList { max-height: 100px; overflow-y: scroll; }
           .selected { background-color: lightblue; }
-          .user { cursor: pointer; padding: 10px; width: 100%; font-size: 14px; }
+          .user { cursor: pointer; padding: 3px; }
         </style>
         <input type="text" id="searchInput" placeholder="Search Users">
         <div id="userList"></div>
@@ -33,44 +34,39 @@ javascript: (function() {
     popupDiv.style.transform = 'translate(-50%, -50%)';
     document.body.appendChild(popupDiv);
 
-    var userList = document.getElementById('userList');
+    var userListDiv = document.getElementById('userList');
     users.entities.forEach(user => {
       var userDiv = document.createElement('div');
       userDiv.className = 'user';
       userDiv.textContent = user.fullname;
       userDiv.onclick = function() {
-        fetchRolesForUser(user.systemuserid, function(roles) {
-          var rolesList = document.getElementById('roles');
-          rolesList.innerHTML = '';
-          roles.entities.forEach(role => {
-            var roleId = role['roleid'];
-            Xrm.WebApi.retrieveRecord("role", roleId, "?$select=name,roleid").then(function(roleDetail) {
-              var listItem = document.createElement('li');
-              listItem.textContent = roleDetail.name;
-              rolesList.appendChild(listItem);
-            });
+        selectUser(user);
+      };
+      userListDiv.appendChild(userDiv);
+    });
+
+    function selectUser(user) {
+      document.querySelectorAll('.user').forEach(el => el.classList.remove('selected'));
+      userListDiv.querySelector(`[data-id='${user.systemuserid}']`).classList.add('selected');
+      fetchRolesForUser(user.systemuserid, function(roles) {
+        var rolesList = document.getElementById('roles');
+        rolesList.innerHTML = '';
+        roles.entities.forEach(role => {
+          var roleId = role['roleid'];
+          Xrm.WebApi.retrieveRecord("role", roleId, "?$select=name,roleid").then(function(roleDetail) {
+            var listItem = document.createElement('li');
+            listItem.textContent = roleDetail.name;
+            rolesList.appendChild(listItem);
           });
         });
-        document.querySelectorAll('.user').forEach(elem => elem.classList.remove('selected'));
-        userDiv.classList.add('selected');
-      };
-      userList.appendChild(userDiv);
-    });
+      });
+    }
 
     document.getElementById('searchInput').oninput = function() {
       var searchValue = this.value.toLowerCase();
-      var userDivs = document.querySelectorAll('.user');
-      userDivs.forEach(div => {
-        if (searchValue === '' || div.textContent.toLowerCase().includes(searchValue)) {
-          div.style.display = 'block';
-        } else {
-          div.style.display = 'none';
-        }
+      document.querySelectorAll('.user').forEach(el => {
+        el.style.display = el.textContent.toLowerCase().includes(searchValue) ? 'block' : 'none';
       });
-      if (searchValue === '') {
-        document.getElementById('roles').innerHTML = '';
-        document.querySelectorAll('.user').forEach(elem => elem.classList.remove('selected'));
-      }
     };
   }
 
