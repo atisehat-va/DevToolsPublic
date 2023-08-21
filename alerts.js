e8a7ff8dbb9087c2c1d7d67a8504ac03719b78e7
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-21 15:01:32 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,68 +1,68 @@
 javascript: (function() {
-  function fetchUsers(callback) {
-    Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname').then(callback);
-  }
+  // Include Select2 CSS
+  var link = document.createElement('link');
+  link.href = 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.1/css/select2.min.css';
+  link.type = 'text/css';
+  link.rel = 'stylesheet';
+  document.head.appendChild(link);
 
-  function getUserSecurityRoles(userId) {
-    var userRolesUrl = `systemusers(${userId})/systemuserroles_association?$select=roleid`;
-    Xrm.WebApi.retrieveMultipleRecords("systemuserroles", `?$filter=systemuserid eq ${userId}`).then(
-      function (result) {
-        var roleDetailPromises = [];
-        result.entities.forEach(function (userRole) {
-          var roleId = userRole["roleid"];
-          roleDetailPromises.push(Xrm.WebApi.retrieveRecord("role", roleId, "?$select=name,roleid"));
+  // Include Select2 JS
+  var script = document.createElement('script');
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.1/js/select2.min.js';
+  document.body.appendChild(script);
+
+  // Wait for the Select2 library to load
+  setTimeout(function() {
+    function fetchUsers(callback) {
+      Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname').then(callback);
+    }
+
+    function fetchRolesForUser(userId, callback) {
+      Xrm.WebApi.retrieveMultipleRecords('systemuserroles', '?$filter=systemuserid eq ' + userId).then(callback);
+    }
+
+    function displayPopup(users) {
+      var popupHtml = `
+      <div class="popup">
+        <style>
+          .popup { display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: white; border: 1px solid #888; padding: 20px; width: 300px; }
+          #userSelect { width: 100%; } /* Set the width of the dropdown */
+        </style>
+        <select id="userSelect">`;
+      users.entities.forEach(user => {
+        popupHtml += `<option value="${user.systemuserid}">${user.fullname}</option>`;
+      });
+      popupHtml += `</select><ul id="roles"></ul></div>`;
+
+      var popupDiv = document.createElement('div');
+      popupDiv.id = 'bookmarkletPopup';
+      popupDiv.innerHTML = popupHtml;
+      popupDiv.style.position = 'absolute';
+      popupDiv.style.zIndex = '10000';
+      popupDiv.style.left = '50%';
+      popupDiv.style.top = '50%';
+      popupDiv.style.transform = 'translate(-50%, -50%)';
+      document.body.appendChild(popupDiv);
+
+      // Convert the dropdown into Select2 searchable dropdown
+      $('#userSelect').select2();
+
+      document.getElementById('userSelect').onchange = function() {
+        var userId = this.value;
+        fetchRolesForUser(userId, function(roles) {
+          var rolesList = document.getElementById('roles');
+          rolesList.innerHTML = '';
+          roles.entities.forEach(role => {
+            var listItem = document.createElement('li');
+            listItem.textContent = role.name;
+            rolesList.appendChild(listItem);
+          });
         });
-        Promise.all(roleDetailPromises).then(
-          function (roleDetails) {
-            var rolesList = '<ul>';
-            roleDetails.forEach(function (roleDetail) {
-              rolesList += `<li>${roleDetail.name}</li>`;
-            });
-            rolesList += '</ul>';
-            document.getElementById('roles').innerHTML = rolesList;
-            console.log("User's Security Roles:", roleDetails.map(roleDetail => roleDetail.name));
-          },
-          function (error) {
-            console.log("Error fetching role details: " + error.message);
-          }
-        );
-      },
-      function (error) {
-        console.log("Error fetching user roles: " + error.message);
-      }
-    );
-  }
+      };
+    }
 
-  function displayPopup(users) {
-    var popupHtml = `
-    <div class="popup">
-      <style>
-        .popup { display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: white; border: 1px solid #888; padding: 20px; width: 300px; }
-        #userSelect { width: 100%; } /* Set the width of the dropdown */
-      </style>
-      <select id="userSelect">`;
-    users.entities.forEach(user => {
-      popupHtml += `<option value="${user.systemuserid}">${user.fullname}</option>`;
+    fetchUsers(function(users) {
+      displayPopup(users);
     });
-    popupHtml += `</select><div id="roles"></div></div>`;
-
-    var popupDiv = document.createElement('div');
-    popupDiv.id = 'bookmarkletPopup';
-    popupDiv.innerHTML = popupHtml;
-    popupDiv.style.position = 'absolute';
-    popupDiv.style.zIndex = '10000';
-    popupDiv.style.left = '50%';
-    popupDiv.style.top = '50%';
-    popupDiv.style.transform = 'translate(-50%, -50%)';
-    document.body.appendChild(popupDiv);
-
-    document.getElementById('userSelect').onchange = function() {
-      var userId = this.value;
-      getUserSecurityRoles(userId);
-    };
-  }
-
-  fetchUsers(function(users) {
-    displayPopup(users);
-  });
+  }, 1000); // Waiting for 1 second for the Select2 library to load
 })();
