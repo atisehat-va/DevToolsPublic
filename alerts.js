87cfc8048b1db0561f9bd570298cfc2c5aacffa5
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-21 12:50:00 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,59 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Security Roles</title>
-</head>
-<body onload="populateUsersDropdown()">
-    <h1>Select a User</h1>
-    <button onclick="populateUsersDropdown();">Load Users</button>
-    <select id="usersDropdown" onchange="showSecurityRoles(this.value)">
-        <option value="">Select a User</option>
-    </select>
-    <div id="securityRolesDisplay"></div>
-
-    <button onclick="showDirtyFields();">Show Modified Fields</button>
-
-    <script>
-        function populateUsersDropdown() {
-            // Query users from Dynamics 365
-            Xrm.WebApi.retrieveMultipleRecords("SystemUser").then(function(users) {
-                var select = document.getElementById("usersDropdown");
-                select.innerHTML = '<option value="">Select a User</option>'; // Clear existing options
-                users.entities.forEach(function(user) {
-                    var option = document.createElement("option");
-                    option.value = user.systemuserid;
-                    option.text = user.fullname;
-                    select.add(option);
-                });
-            });
-        }
-
-        function showSecurityRoles(userId) {
-            if (!userId) {
-                // Handle empty selection
-                return;
-            }
-
-            // Query the security roles for the selected user
-            var query = "/SystemUsers(" + userId + ")/systemuserroles_association?$select=name";
-            Xrm.WebApi.retrieveMultipleRecords("Role", query).then(function(roles) {
-                var html = '<h2>User Security Roles</h2>';
-                roles.entities.forEach(function(role) {
-                    html += '<div>' + role.name + '</div>';
-                });
-
-                // Display the HTML content
-                document.getElementById("securityRolesDisplay").innerHTML = html;
-            });
-        }        
-    </script>
-</body>
-</html>
-//------------------
-
 javascript: (function() {
   function fetchUsersAndRoles(callback) {
     Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=fullname').then(function(users) {
@@ -69,33 +13,52 @@ javascript: (function() {
         promises.push(promise);
       });
 
-      // Wait for all promises to resolve
       Promise.all(promises).then(function() {
         callback(userRoles);
       });
     });
   }
 
-  function displayPopup(users) {
-    var html = '<select id="userSelect">';
+  function openPopup(users) {
+    var popupHtml = `
+      <style>
+        /* ... same styles as your provided popup ... */
+      </style>
+      <div class="popup">
+        <div class="button-container">
+          <select id="userSelect">
+    `;
     users.forEach(user => {
-      html += '<option value="' + user.name + '">' + user.name + '</option>';
+      popupHtml += '<option value="' + user.name + '">' + user.name + '</option>';
     });
-    html += '</select><div id="roles"></div>';
+    popupHtml += `
+          </select>
+          <div id="roles"></div>
+          <button onclick="document.getElementById('bookmarkletPopup').remove();">Close</button>
+        </div>
+      </div>
+    `;
 
-    var popup = window.open('', 'popup', 'width=300,height=200');
-    popup.document.write(html);
-    popup.document.close(); // Close the document to allow further manipulation
+    var popupDiv = document.createElement('div');
+    popupDiv.id = 'bookmarkletPopup';
+    popupDiv.innerHTML = popupHtml;
+    popupDiv.style.position = 'absolute';
+    popupDiv.style.zIndex = '10000';
+    popupDiv.style.left = '50%';
+    popupDiv.style.top = '50%';
+    popupDiv.style.transform = 'translate(-50%, -50%)';
+    popupDiv.style.backgroundColor = 'white';
 
-    popup.document.getElementById('userSelect').onchange = function() {
+    document.body.appendChild(popupDiv);
+
+    document.getElementById('userSelect').onchange = function() {
       var userName = this.value;
       var roles = users.find(user => user.name === userName).roles;
-      popup.document.getElementById('roles').innerHTML = roles.join(', ');
+      document.getElementById('roles').innerHTML = roles.join(', ');
     };
   }
 
   fetchUsersAndRoles(function(users) {
-    displayPopup(users);
+    openPopup(users);
   });
 })();
-
