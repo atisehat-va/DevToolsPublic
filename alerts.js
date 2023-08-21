a88e3fca0b4a1457d53605edde28b443a7338e65
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-21 15:06:58 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,34 +1,37 @@
 javascript: (function() {
-  // Include Select2 CSS
+  // Include the Select2 CSS
   var link = document.createElement('link');
-  link.href = 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.1/css/select2.min.css';
-  link.type = 'text/css';
   link.rel = 'stylesheet';
+  link.href = 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css';
   document.head.appendChild(link);
 
-  // Include Select2 JS
+  // Include the Select2 JS
   var script = document.createElement('script');
-  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-beta.1/js/select2.min.js';
-  document.body.appendChild(script);
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js';
+  document.head.appendChild(script);
 
-  // Wait for the Select2 library to load
+  // Delay the execution to ensure Select2 is loaded
   setTimeout(function() {
     function fetchUsers(callback) {
       Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname').then(callback);
     }
 
     function fetchRolesForUser(userId, callback) {
-      Xrm.WebApi.retrieveMultipleRecords('systemuserroles', '?$filter=systemuserid eq ' + userId).then(callback);
+      var userRolesUrl = `systemusers(${userId})/systemuserroles_association?$select=roleid`;
+      Xrm.WebApi.retrieveMultipleRecords('systemuserroles', `?$filter=systemuserid eq ${userId}`).then(callback);
     }
 
     function displayPopup(users) {
+      // Sort the users alphabetically
+      users.entities.sort((a, b) => a.fullname.localeCompare(b.fullname));
+
       var popupHtml = `
-      <div class="popup">
-        <style>
-          .popup { display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: white; border: 1px solid #888; padding: 20px; width: 300px; }
-          #userSelect { width: 100%; } /* Set the width of the dropdown */
-        </style>
-        <select id="userSelect">`;
+        <div class="popup">
+          <style>
+            .popup { display: flex; flex-direction: column; align-items: center; justify-content: center; background-color: white; border: 1px solid #888; padding: 20px; width: 300px; }
+            #userSelect { width: 100%; } /* Set the width of the dropdown */
+          </style>
+          <select id="userSelect">`;
       users.entities.forEach(user => {
         popupHtml += `<option value="${user.systemuserid}">${user.fullname}</option>`;
       });
@@ -44,18 +47,21 @@ javascript: (function() {
       popupDiv.style.transform = 'translate(-50%, -50%)';
       document.body.appendChild(popupDiv);
 
-      // Convert the dropdown into Select2 searchable dropdown
+      // Initialize Select2 on the dropdown
       $('#userSelect').select2();
 
       document.getElementById('userSelect').onchange = function() {
         var userId = this.value;
         fetchRolesForUser(userId, function(roles) {
           var rolesList = document.getElementById('roles');
-          rolesList.innerHTML = '';
+          rolesList.innerHTML = ''; // Clear previous roles
           roles.entities.forEach(role => {
-            var listItem = document.createElement('li');
-            listItem.textContent = role.name;
-            rolesList.appendChild(listItem);
+            var roleId = role['roleid'];
+            Xrm.WebApi.retrieveRecord("role", roleId, "?$select=name,roleid").then(function(roleDetail) {
+              var listItem = document.createElement('li');
+              listItem.textContent = roleDetail.name;
+              rolesList.appendChild(listItem);
+            });
           });
         });
       };
@@ -64,5 +70,5 @@ javascript: (function() {
     fetchUsers(function(users) {
       displayPopup(users);
     });
-  }, 1000); // Waiting for 1 second for the Select2 library to load
+  }, 4000); // 1-second delay to ensure Select2 is loaded
 })();
