bc041dedb6c4875d9657e3810b1d2fc994d8c199
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-22 12:42:51 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,27 +1,27 @@
-javascript: (function() {
+(function() {
+  const popupCss = `
+    .popup { background-color: white; border: 2px solid #444; border-radius: 10px; width: 900px; height: 600px; overflow: hidden; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); }
+    .section { padding: 20px; border-right: 1px solid #ccc; text-align: left; overflow-y: scroll; }
+    #section1 { text-align: center; height: 200px; }
+    #section2, #section3, #section4 { display: inline-block; width: 33%; height: 300px; vertical-align: top; box-sizing: border-box; }
+    #userList { max-height: 150px; overflow-y: scroll; }
+    .selected { background-color: #f0f0f0; }
+    .user { cursor: pointer; padding: 3px; font-size: 14px; }
+    #sectionsRow { white-space: nowrap; }
+  `;
+
   function fetchUsers(callback) {
-    Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname').then(callback);
+    Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname&$filter=statuscode eq 1').then(callback);
   }
 
   function fetchRolesForUser(userId, callback) {
-    var userRolesUrl = `systemusers(${userId})/systemuserroles_association?$select=roleid`;
     Xrm.WebApi.retrieveMultipleRecords('systemuserroles', `?$filter=systemuserid eq ${userId}`).then(callback);
   }
 
-  function displayPopup(users) {
-    users.entities.sort((a, b) => a.fullname.localeCompare(b.fullname));
-    var popupHtml = `
+  function createPopupHtml() {
+    return `
       <div class="popup">
-        <style>
-          .popup { background-color: white; border: 1px solid #888; width: 900px; height: 500px; overflow: hidden; }
-          .section { padding: 20px; border: 1px solid #888; text-align: left; overflow-y: scroll; }
-          #section1 { text-align: center; height: 200px;}
-          #section2, #section3, #section4 { display: inline-block; width: 33%; height: 300px; vertical-align: top; box-sizing: border-box; }
-          #userList { max-height: 150px; overflow-y: scroll; }
-          .selected { background-color: lightblue; }
-          .user { cursor: pointer; padding: 3px; }
-          #sectionsRow { white-space: nowrap; }
-        </style>
+        <style>${popupCss}</style>
         <div class="section" id="section1">
           <h3>Section 1</h3><br>
           <input type="text" id="searchInput" placeholder="Search Users">
@@ -30,13 +30,14 @@ javascript: (function() {
         <div id="sectionsRow">
           <div class="section" id="section2"><h3>Section 2</h3></div>
           <div class="section" id="section3"><h3>Section 3</h3></div>
-          <div class="section" id="section4"><h3>Section 4</h3>
-            <ul></ul>
-          </div>
+          <div class="section" id="section4"><h3>Section 4</h3><ul></ul></div>
         </div>
       </div>`;
+  }
 
-    var popupDiv = document.createElement('div');
+  function createAndAppendPopup() {
+    const popupHtml = createPopupHtml();
+    const popupDiv = document.createElement('div');
     popupDiv.id = 'bookmarkletPopup';
     popupDiv.innerHTML = popupHtml;
     popupDiv.style.position = 'absolute';
@@ -45,45 +46,56 @@ javascript: (function() {
     popupDiv.style.top = '50%';
     popupDiv.style.transform = 'translate(-50%, -50%)';
     document.body.appendChild(popupDiv);
+    return popupDiv;
+  }
 
-    var userListDiv = document.getElementById('userList');
-    users.entities.forEach(user => {
-      var userDiv = document.createElement('div');
+  function renderUserList(users, selectUserCallback) {
+    const userListDiv = document.getElementById('userList');
+    users.forEach(user => {
+      const userDiv = document.createElement('div');
       userDiv.className = 'user';
       userDiv.textContent = user.fullname;
       userDiv.dataset.id = user.systemuserid;
-      userDiv.onclick = function() {
-        selectUser(user);
-      };
+      userDiv.onclick = () => selectUserCallback(user);
       userListDiv.appendChild(userDiv);
     });
+  }
 
-    function selectUser(user) {
-      document.querySelectorAll('.user').forEach(el => el.classList.remove('selected'));
-      userListDiv.querySelector(`[data-id='${user.systemuserid}']`).classList.add('selected');
-      fetchRolesForUser(user.systemuserid, function(roles) {
-        var rolesList = document.getElementById('section4').querySelector('ul');
-        rolesList.innerHTML = '';
-        roles.entities.forEach(role => {
-          var roleId = role['roleid'];
-          Xrm.WebApi.retrieveRecord("role", roleId, "?$select=name,roleid").then(function(roleDetail) {
-            var listItem = document.createElement('li');
-            listItem.textContent = roleDetail.name;
-            rolesList.appendChild(listItem);
-          });
+  function selectUser(user) {
+    document.querySelectorAll('.user').forEach(el => el.classList.remove('selected'));
+    const userDiv = document.getElementById('userList').querySelector(`[data-id='${user.systemuserid}']`);
+    userDiv.classList.add('selected');
+    fetchRolesForUser(user.systemuserid, function(roles) {
+      const rolesList = document.getElementById('section4').querySelector('ul');
+      rolesList.innerHTML = '';
+      roles.entities.forEach(role => {
+        const roleId = role['roleid'];
+        Xrm.WebApi.retrieveRecord("role", roleId, "?$select=name,roleid").then(function(roleDetail) {
+          const listItem = document.createElement('li');
+          listItem.textContent = roleDetail.name;
+          rolesList.appendChild(listItem);
         });
       });
-    }
+    });
+  }
 
+  function setupSearchFilter() {
     document.getElementById('searchInput').oninput = function() {
-      var searchValue = this.value.toLowerCase();
+      const searchValue = this.value.toLowerCase();
       document.querySelectorAll('.user').forEach(el => {
         el.style.display = el.textContent.toLowerCase().includes(searchValue) ? 'block' : 'none';
       });
     };
   }
 
+  function displayPopup(users) {
+    users.entities.sort((a, b) => a.fullname.localeCompare(b.fullname));
+    createAndAppendPopup();
+    renderUserList(users.entities, selectUser);
+    setupSearchFilter();
+  }
+
   fetchUsers(function(users) {
     displayPopup(users);
   });
-})(); // code reviewed
+})();
