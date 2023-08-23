d8ed8188a8aa379f2b087feb2b4c38d02f47d1b1
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-23 14:04:18 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -131,7 +131,7 @@ javascript: (function() {
   });
 })();
 
-//----------------------NEW--Test-----------
+//----------------------NEW working--Test-----------
 function updateUserDetails(userId, businessUnitId, teamId, roleId) {
     // Change Business Unit
     var data1 = {
@@ -190,5 +190,66 @@ function updateUserDetails(userId, businessUnitId, teamId, roleId) {
 
 // Usage
 updateUserDetails("<USER_ID>", "<BUSINESS_UNIT_ID>", "<TEAM_ID>", "<ROLE_ID>");
+//---------------------NEW NEW--------------
+function updateUserDetails(userId, businessUnitId, teamId, roleId) {
+    // Change Business Unit
+    var data1 = {
+        "businessunitid@odata.bind": "/businessunits(" + businessUnitId + ")"
+    };
+    Xrm.WebApi.updateRecord("systemuser", userId, data1).then(function() {
+        // Disassociate User from Current Teams
+        var teamsUrl = Xrm.Utility.getGlobalContext().getClientUrl() + "/api/data/v9.0/systemusers(" + userId + ")/teammembership_association";
+        var teamRequest = new XMLHttpRequest();
+        teamRequest.open("GET", teamsUrl, true);
+        teamRequest.setRequestHeader("OData-MaxVersion", "4.0");
+        teamRequest.setRequestHeader("OData-Version", "4.0");
+        teamRequest.setRequestHeader("Accept", "application/json");
+        teamRequest.onreadystatechange = function() {
+            if (this.readyState === 4) {
+                teamRequest.onreadystatechange = null;
+                if (this.status === 200) {
+                    var results = JSON.parse(this.response).value;
+                    results.forEach(function(result) {
+                        var disassociateUrl = Xrm.Utility.getGlobalContext().getClientUrl() + "/api/data/v9.0/systemusers(" + userId + ")/teammembership_association/$ref?$id=" + Xrm.Utility.getGlobalContext().getClientUrl() + "/api/data/v9.0/teams(" + result.teamid + ")";
+                        var disassociateRequest = new XMLHttpRequest();
+                        disassociateRequest.open("DELETE", disassociateUrl, true);
+                        disassociateRequest.send();
+                    });
+                }
+            }
+        };
+        teamRequest.send();
+
+        // Associate User to Specified Team
+        var associateTeamUrl = Xrm.Utility.getGlobalContext().getClientUrl() + "/api/data/v9.0/teams(" + teamId + ")/teammembership_association/$ref";
+        var associateTeamData = {
+            "@odata.id": Xrm.Utility.getGlobalContext().getClientUrl() + "/api/data/v9.0/systemusers(" + userId + ")"
+        };
+        var associateTeamRequest = new XMLHttpRequest();
+        associateTeamRequest.open("POST", associateTeamUrl, true);
+        associateTeamRequest.setRequestHeader("OData-MaxVersion", "4.0");
+        associateTeamRequest.setRequestHeader("OData-Version", "4.0");
+        associateTeamRequest.setRequestHeader("Accept", "application/json");
+        associateTeamRequest.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+        associateTeamRequest.send(JSON.stringify(associateTeamData));
+
+        // Associate User to Specified Role
+        var associateRoleUrl = Xrm.Utility.getGlobalContext().getClientUrl() + "/api/data/v9.0/roles(" + roleId + ")/systemuserroles_association/$ref";
+        var associateRoleData = {
+            "@odata.id": Xrm.Utility.getGlobalContext().getClientUrl() + "/api/data/v9.0/systemusers(" + userId + ")"
+        };
+        var associateRoleRequest = new XMLHttpRequest();
+        associateRoleRequest.open("POST", associateRoleUrl, true);
+        associateRoleRequest.setRequestHeader("OData-MaxVersion", "4.0");
+        associateRoleRequest.setRequestHeader("OData-Version", "4.0");
+        associateRoleRequest.setRequestHeader("Accept", "application/json");
+        associateRoleRequest.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+        associateRoleRequest.send(JSON.stringify(associateRoleData));
+    });
+}
+
+// Usage
+updateUserDetails("<USER_ID>", "<BUSINESS_UNIT_ID>", "<TEAM_ID>", "<ROLE_ID>");
+
 
 
