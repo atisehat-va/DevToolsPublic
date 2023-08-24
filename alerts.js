6034b86a15a649fc77ac198321be6b944db21d53
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-24 11:36:50 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -3,14 +3,17 @@ javascript: (function() {
   const popupCss = `
     .popup { background-color: white; border: 2px solid #444; border-radius: 10px; width: 700px; height: 500px; overflow: hidden; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); }
     .section { padding: 20px; border-right: 1px solid #ccc; overflow-y: scroll; }
-    .section h3 { text-align: left; margin-bottom: 10px; } /* Align titles to the left */
+    .section h3 { text-align: left; margin-bottom: 10px; } /* Align the titles to the left */
     #section1 { text-align: center; height: 220px; }
     #section1 input { margin-bottom: 10px; width: 230px;}
     #section1 #userList { margin-bottom: 15px; max-height: 130px; overflow-y: scroll; scrollbar-width: none; -ms-overflow-style: none; }
     #section1 #userList::-webkit-scrollbar { display: none; }
-    #section2, #section3 { display: inline-block; width: 50%; height: 250px; vertical-align: top; box-sizing: border-box; text-align: left; }
+    #section2, #section3, #section4 { display: inline-block; width: 33%; height: 250px; vertical-align: top; box-sizing: border-box; text-align: left; }
+    .selected { background-color: #f0f0f0; }
+    .user { cursor: pointer; padding: 3px; font-size: 14px; }
     #sectionsRow { white-space: nowrap; }
-    .indented { margin-left: 20px; } /* Indent corresponding text */
+    #section2 ul li, #section4 ul li { list-style-type: none; padding-left: 20px; position: relative; } /* Custom styling for list items */
+    #section2 ul li:before, #section4 ul li:before { content: '•'; position: absolute; left: 0; } /* Adding bullet mark */
   `;
 
   function fetchUsers(callback) {
@@ -39,11 +42,9 @@ javascript: (function() {
           <div id="userList"></div>
         </div>
         <div id="sectionsRow">
-          <div class="section" id="section2">
-            <h3>Business Unit</h3><ul class="indented"></ul> <!-- Indent the text -->
-            <h3>Teams</h3><ul id="teamList" class="indented"></ul> <!-- Indent the text -->
-          </div>
-          <div class="section" id="section3"><h3>Security Roles</h3><ul class="indented"></ul></div> <!-- Indent the text -->
+          <div class="section" id="section2"><h3>Business Unit</h3><ul></ul></div>
+          <div class="section" id="section3"><h3>Teams</h3><ul></ul></div>
+          <div class="section" id="section4"><h3>Security Roles</h3><ul></ul></div>
         </div>
       </div>`;
   }
@@ -102,7 +103,7 @@ javascript: (function() {
     });
 
     fetchTeamsForUser(user.systemuserid, function(response) {
-      const teamsList = document.getElementById('teamList');
+      const teamsList = document.getElementById('section3').querySelector('ul');
       teamsList.innerHTML = '';
       response.entities[0].teammembership_association.forEach(team => {
         const listItem = document.createElement('li');
