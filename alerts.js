402ebbd9d998e26839ccbb7fbff99fce7ad2620d
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-24 12:18:58 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -3,14 +3,13 @@ javascript: (function() {
   const popupCss = `
     .popup { background-color: white; border: 2px solid #444; border-radius: 10px; width: 700px; height: 500px; overflow: hidden; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); }
     .section { padding: 20px; border-right: 1px solid #ccc; overflow-y: scroll; }
-    .section h3 { margin-bottom: 10px; text-align: left; } /* Left alignment for all headers */
-    #section1 h3, #section1 { text-align: center; } /* Center alignment for User Info */
+    .section h3 { text-align: left; margin-bottom: 10px; } /* Aligns the section titles to the left */
+    .section ul { padding-left: 20px; } /* Indents the corresponding text */
+    #section1 { text-align: center; height: 220px; }
     #section1 input { margin-bottom: 10px; width: 230px;}
     #section1 #userList { margin-bottom: 15px; max-height: 130px; overflow-y: scroll; scrollbar-width: none; -ms-overflow-style: none; }
     #section1 #userList::-webkit-scrollbar { display: none; }
-    #section2 ul li, #section3 ul li { list-style-type: none; padding-left: 30px; position: relative; } /* Custom styling for list items with indentation */
-    #section2 ul li:before, #section3 ul li:before { content: '•'; position: absolute; left: 10px; } /* Adding bullet mark */
-    #section2, #section3 { display: inline-block; width: 50%; height: 250px; vertical-align: top; box-sizing: border-box; text-align: left; } /* Updated width */
+    #section2, #section3, #section4 { display: inline-block; width: 33%; height: 250px; vertical-align: top; box-sizing: border-box; text-align: left; }
     .selected { background-color: #f0f0f0; }
     .user { cursor: pointer; padding: 3px; font-size: 14px; }
     #sectionsRow { white-space: nowrap; }
@@ -42,11 +41,9 @@ javascript: (function() {
           <div id="userList"></div>
         </div>
         <div id="sectionsRow">
-          <div class="section" id="section2">
-            <h3>Business Unit</h3><ul id="businessUnit"></ul>
-            <h3>Teams</h3><ul id="teams"></ul>
-          </div>
-          <div class="section" id="section3"><h3>Security Roles</h3><ul></ul></div>
+          <div class="section" id="section2"><h3>Business Unit</h3><ul></ul></div>
+          <div class="section" id="section3"><h3>Teams</h3><ul></ul></div>
+          <div class="section" id="section4"><h3>Security Roles</h3><ul></ul></div>
         </div>
       </div>`;
   }
@@ -78,43 +75,42 @@ javascript: (function() {
   }
 
   function selectUser(user) {
-  document.querySelectorAll('.user').forEach(el => el.classList.remove('selected'));
-  const userDiv = document.getElementById('userList').querySelector(`[data-id='${user.systemuserid}']`);
-  userDiv.classList.add('selected');
+    document.querySelectorAll('.user').forEach(el => el.classList.remove('selected'));
+    const userDiv = document.getElementById('userList').querySelector(`[data-id='${user.systemuserid}']`);
+    userDiv.classList.add('selected');
+    
+    const businessUnitList = document.getElementById('section2').querySelector('ul');
+    businessUnitList.innerHTML = '';
 
-  const businessUnitList = document.getElementById('businessUnit');
-  businessUnitList.innerHTML = '';
+    fetchBusinessUnitName(user._businessunitid_value, function(businessUnit) {
+      const listItem = document.createElement('li');
+      listItem.textContent = businessUnit.name;
+      businessUnitList.appendChild(listItem);
+    });
 
-  fetchBusinessUnitName(user._businessunitid_value, function(businessUnit) {
-    const listItem = document.createElement('li');
-    listItem.textContent = businessUnit.name;
-    businessUnitList.appendChild(listItem);
-  });
-
-  const rolesList = document.getElementById('section3').querySelector('ul'); // Get the Security Roles list
-  rolesList.innerHTML = '';
-
-  fetchRolesForUser(user.systemuserid, function(roles) {
-    roles.entities.forEach(role => {
-      const roleId = role['roleid'];
-      Xrm.WebApi.retrieveRecord("role", roleId, "?$select=name,roleid").then(function(roleDetail) {
-        const listItem = document.createElement('li');
-        listItem.textContent = roleDetail.name;
-        rolesList.appendChild(listItem); // Append to the Security Roles list
+    fetchRolesForUser(user.systemuserid, function(roles) {
+      const rolesList = document.getElementById('section4').querySelector('ul');
+      rolesList.innerHTML = '';
+      roles.entities.forEach(role => {
+        const roleId = role['roleid'];
+        Xrm.WebApi.retrieveRecord("role", roleId, "?$select=name,roleid").then(function(roleDetail) {
+          const listItem = document.createElement('li');
+          listItem.textContent = roleDetail.name;
+          rolesList.appendChild(listItem);
+        });
       });
     });
-  });
 
-  fetchTeamsForUser(user.systemuserid, function(response) {
-    const teamsList = document.getElementById('teams');
-    teamsList.innerHTML = '';
-    response.entities[0].teammembership_association.forEach(team => {
-      const listItem = document.createElement('li');
-      listItem.textContent = team.name;
-      teamsList.appendChild(listItem);
+    fetchTeamsForUser(user.systemuserid, function(response) {
+      const teamsList = document.getElementById('section3').querySelector('ul');
+      teamsList.innerHTML = '';
+      response.entities[0].teammembership_association.forEach(team => {
+        const listItem = document.createElement('li');
+        listItem.textContent = team.name;
+        teamsList.appendChild(listItem);
+      });
     });
-  });
-}
+  }
 
   function setupSearchFilter() {
     document.getElementById('searchInput').oninput = function() {
