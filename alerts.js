72b2102494b078b29473ac83efb81620ffa4408e
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-24 12:49:04 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -3,13 +3,12 @@ javascript: (function() {
   const popupCss = `
     .popup { background-color: white; border: 2px solid #444; border-radius: 10px; width: 700px; height: 500px; overflow: hidden; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); }
     .section { padding: 20px; border-right: 1px solid #ccc; overflow-y: scroll; }
-    .section h3 { text-align: left; margin-bottom: 10px; }
-    .section ul { padding-left: 20px; }
+    .section h3 { text-align: center; margin-bottom: 10px; }
     #section1 { text-align: center; height: 220px; }
     #section1 input { margin-bottom: 10px; width: 230px;}
     #section1 #userList { margin-bottom: 15px; max-height: 130px; overflow-y: scroll; scrollbar-width: none; -ms-overflow-style: none; }
     #section1 #userList::-webkit-scrollbar { display: none; }
-    #section2, #section3 { display: inline-block; width: 50%; height: 250px; vertical-align: top; box-sizing: border-box; text-align: left; }
+    #section2, #section3, #section4 { display: inline-block; width: 33%; height: 250px; vertical-align: top; box-sizing: border-box; text-align: left; }
     .selected { background-color: #f0f0f0; }
     .user { cursor: pointer; padding: 3px; font-size: 14px; }
     #sectionsRow { white-space: nowrap; }
@@ -17,6 +16,49 @@ javascript: (function() {
 
   function fetchUsers(callback) {
     Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname,_businessunitid_value&$filter=(isdisabled eq false)').then(callback);
+  }
+
+  function fetchRolesForUser(userId, callback) {
+    Xrm.WebApi.retrieveMultipleRecords('systemuserroles', `?$filter=systemuserid eq ${userId}`).then(callback);
+  }
+
+  function fetchTeamsForUser(userId, callback) {
+    Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=teammembership_association($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+  }
+
+  function fetchBusinessUnitName(businessUnitId, callback) {
+    Xrm.WebApi.retrieveRecord('businessunit', businessUnitId, '?$select=name').then(callback);
+  }
+
+  function createPopupHtml() {
+    return `
+      <div class="popup">
+        <style>${popupCss}</style>
+        <div class="section" id="section1">
+          <h3>User Info</h3>
+          <input type="text" id="searchInput" placeholder="Search Users">
+          <div id="userList"></div>
+        </div>
+        <div id="sectionsRow">
+          <div class="section" id="section2"><h3>Business Unit</h3><ul></ul></div>
+          <div class="section" id="section3"><h3>Teams</h3><ul></ul></div>
+          <div class="section" id="section4"><h3>Security Roles</h3><ul></ul></div>
+        </div>
+      </div>`;
+  }
+
+  function createAndAppendPopup() {
+    const popupHtml = createPopupHtml();
+    const popupDiv = document.createElement('div');
+    popupDiv.id = 'bookmarkletPopup';
+    popupDiv.innerHTML = popupHtml;
+    popupDiv.style.position = 'absolute';
+    popupDiv.style.zIndex = '10000';
+    popupDiv.style.left = '50%';
+    popupDiv.style.top = '50%';
+    popupDiv.style.transform = 'translate(-50%, -50%)';
+    document.body.appendChild(popupDiv);
+    return popupDiv;
   }
 
   function renderUserList(users, selectUserCallback) {
@@ -45,36 +87,28 @@ javascript: (function() {
       businessUnitList.appendChild(listItem);
     });
 
-  function createPopupHtml() {
-    return `
-      <div class="popup">
-        <style>${popupCss}</style>
-        <div class="section" id="section1">
-          <h3>User Info</h3>
-          <input type="text" id="searchInput" placeholder="Search Users">
-          <div id="userList"></div>
-        </div>
-        <div id="sectionsRow">
-          <div class="section" id="section2">
-            <h3>Business Unit</h3><ul></ul>
-            <h3>Teams</h3><ul></ul>
-          </div>
-          <div class="section" id="section3"><h3>Security Roles</h3><ul></ul></div>
-        </div>
-      </div>`;
-  }
+    fetchRolesForUser(user.systemuserid, function(roles) {
+      const rolesList = document.getElementById('section4').querySelector('ul');
+      rolesList.innerHTML = '';
+      roles.entities.forEach(role => {
+        const roleId = role['roleid'];
+        Xrm.WebApi.retrieveRecord("role", roleId, "?$select=name,roleid").then(function(roleDetail) {
+          const listItem = document.createElement('li');
+          listItem.textContent = roleDetail.name;
+          rolesList.appendChild(listItem);
+        });
+      });
+    });
 
-  function createAndAppendPopup() {
-    const popupHtml = createPopupHtml();
-    const popupDiv = document.createElement('div');
-    popupDiv.id = 'bookmarkletPopup';
-    popupDiv.innerHTML = popupHtml;
-    popupDiv.style.position = 'absolute';
-    popupDiv.style.zIndex = '10000';
-    popupDiv.style.left = '50%';
-    popupDiv.style.top = '50%';
-    popupDiv.style.transform = 'translate(-50%, -50%)';
-    document.body.appendChild(popupDiv);
+    fetchTeamsForUser(user.systemuserid, function(response) {
+      const teamsList = document.getElementById('section3').querySelector('ul');
+      teamsList.innerHTML = '';
+      response.entities[0].teammembership_association.forEach(team => {
+        const listItem = document.createElement('li');
+        listItem.textContent = team.name;
+        teamsList.appendChild(listItem);
+      });
+    });
   }
 
   function setupSearchFilter() {
@@ -97,8 +131,6 @@ javascript: (function() {
     displayPopup(users);
   });
 })();
-
-
 
 //------------end NEw 08-24-23---------------------------------
 javascript: (function() {
