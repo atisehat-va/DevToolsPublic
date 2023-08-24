7f9ad205f0b44b3a3f6bc7b4f4eff5acec7f7d6a
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-24 15:42:33 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,21 +1,22 @@
 //--------------new-------------------------------------------
 javascript: (function() {
   const popupCss = `
-    .popup { background-color: white; border: 2px solid #444; border-radius: 10px; width: 700px; height: 500px; overflow: hidden; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); }
-    .section { padding: 20px; border-right: 1px solid #ccc; overflow-y: scroll; }
-    .security-btn { position: absolute; top: 10px; right: 10px; background-color: #007bff; color: white; border: none; padding: 8px 16px; font-size: 14px; cursor: pointer; border-radius: 4px; }
-    #section1 { text-align: center; height: 220px; }
-    #section1 input { margin-bottom: 10px; width: 230px;}
-    #section1 #userList { margin-bottom: 15px; max-height: 130px; overflow-y: scroll; scrollbar-width: none; -ms-overflow-style: none; }
-    #section1 #userList::-webkit-scrollbar { display: none; }
-    #section2, #section3 { display: inline-block; width: 50%; height: 250px; vertical-align: top; box-sizing: border-box; text-align: left; }
-    .selected { background-color: #f0f0f0; }
-    .user { cursor: pointer; padding: 3px; font-size: 14px; }
-    #sectionsRow { white-space: nowrap; }
-    #businessUnitList li, #teamsList li, #section3 ul li { margin-left: 20px; }
-    #businessUnitList { margin-bottom: 15px; }
-  `;
-
+  .popup { background-color: white; border: 2px solid #444; border-radius: 10px; width: 700px; height: 500px; overflow: hidden; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); position: relative; }
+  .content { width: 100%; transition: width 0.5s; }
+  .shrunk { width: 50% !important; }
+  .security-btn { position: absolute; top: 10px; right: 10px; background-color: #007bff; color: white; border: none; padding: 8px 16px; font-size: 14px; cursor: pointer; border-radius: 4px; }
+  .section { padding: 20px; border-right: 1px solid #ccc; overflow-y: scroll; }
+  #section1 { text-align: center; height: 220px; }
+  #section1 input { margin-bottom: 10px; width: 230px;}
+  #section1 #userList { margin-bottom: 15px; max-height: 130px; overflow-y: scroll; scrollbar-width: none; -ms-overflow-style: none; }
+  #section1 #userList::-webkit-scrollbar { display: none; }
+  #section2, #section3 { display: inline-block; width: 50%; height: 250px; vertical-align: top; box-sizing: border-box; text-align: left; }
+  .selected { background-color: #f0f0f0; }
+  .user { cursor: pointer; padding: 3px; font-size: 14px; }
+  #sectionsRow { white-space: nowrap; }
+  #businessUnitList li, #teamsList li, #section3 ul li { margin-left: 20px; }
+  #businessUnitList { margin-bottom: 15px; }
+`;
 
   function fetchUsers(callback) {
     Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname,_businessunitid_value&$filter=(isdisabled eq false)').then(callback);
@@ -34,9 +35,10 @@ javascript: (function() {
   }
 
   function createPopupHtml() {
-    return `
-      <div class="popup">
-        <button class="security-btn" onclick="securityFunction()">Security</button>
+  return `
+    <div class="popup">
+      <button class="security-btn" onclick="toggleContent()">Security</button>
+      <div class="content">
         <style>${popupCss}</style>
         <div class="section" id="section1">
           <h3>User Info</h3>
@@ -50,9 +52,13 @@ javascript: (function() {
           </div>
           <div class="section" id="section3"><h3>Security Roles:</h3><ul></ul></div>
         </div>
-      </div>`;
-  }
-
+      </div>
+    </div>`;
+}
+function toggleContent() {
+  const contentDiv = document.querySelector('.content');
+  contentDiv.classList.toggle('shrunk');
+}
   function createAndAppendPopup() {
     const popupHtml = createPopupHtml();
     const popupDiv = document.createElement('div');
