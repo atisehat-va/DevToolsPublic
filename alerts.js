c1f63668fde0b56d07af238b3dee1975b6e630f4
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-25 10:56:06 -0400

    Update alerts.js

--- alerts.js
+++ alerts.js
@@ -1,21 +1,18 @@
 //--------------new-------------------------------------------
 javascript: (function() {
   const popupCss = `
-  .popup { background-color: white; border: 2px solid #444; border-radius: 10px; width: 700px; height: 500px; overflow: hidden; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); position: relative; }
+  .popup { background-color: white; border: 2px solid #444; border-radius: 10px; width: 700px; height: 600px; overflow: hidden; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); position: relative; }
   .content { width: 100%; transition: width 0.5s; }
   .shrunk { width: 50% !important; }
   .security-btn { position: absolute; top: 10px; right: 10px; background-color: #007bff; color: white; border: none; padding: 8px 16px; font-size: 14px; cursor: pointer; border-radius: 4px; }
   .section { padding: 20px; border-right: 1px solid #ccc; overflow-y: scroll; }
-  #section1 { text-align: center; height: 220px; }
-  #section1 input { margin-bottom: 10px; width: 230px;}
-  #section1 #userList { margin-bottom: 15px; max-height: 130px; overflow-y: scroll; scrollbar-width: none; -ms-overflow-style: none; }
-  #section1 #userList::-webkit-scrollbar { display: none; }
-  #section2, #section3 { display: inline-block; width: 50%; height: 250px; vertical-align: top; box-sizing: border-box; text-align: left; }
-  .selected { background-color: #f0f0f0; }
-  .user { cursor: pointer; padding: 3px; font-size: 14px; }
+  #section1, #section2 { text-align: center; height: 220px; }
+  #section1 input, #section2 input { margin-bottom: 10px; width: 230px;}
+  #section1 #userList, #section2 #userList2 { margin-bottom: 15px; max-height: 130px; overflow-y: scroll; scrollbar-width: none; -ms-overflow-style: none; }
+  #section1 #userList::-webkit-scrollbar, #section2 #userList2::-webkit-scrollbar { display: none; }
   #sectionsRow { white-space: nowrap; }
-  #businessUnitList li, #teamsList li, #section3 ul li { margin-left: 20px; }
-  #businessUnitList { margin-bottom: 15px; }
+  #businessUnitList li, #teamsList li, #section3 ul li, #section4 ul li { margin-left: 20px; }
+  #businessUnitList, #businessUnitList2 { margin-bottom: 15px; }
 `;
   function toggleContent() {
     const contentDiv = document.querySelector('.content');
@@ -40,26 +37,31 @@ javascript: (function() {
   }
 
   function createPopupHtml() {
-  return `
-    <div class="popup">
-      <button class="security-btn" onclick="toggleContent()">Security</button>
-      <div class="content">
-        <style>${popupCss}</style>
-        <div class="section" id="section1">
-          <h3>User Info</h3>
-          <input type="text" id="searchInput" placeholder="Search Users">
-          <div id="userList"></div>
-        </div>
-        <div id="sectionsRow">
-          <div class="section" id="section2">
-            <h3>Business Unit:</h3><ul id="businessUnitList"></ul>
-            <h3>Teams:</h3><ul id="teamsList"></ul>
+    return `
+      <div class="popup">
+        <button class="security-btn" onclick="toggleContent()">Security</button>
+        <div class="content">
+          <style>${popupCss}</style>
+          <div class="section" id="section1">
+            <h3>User Info</h3>
+            <input type="text" id="searchInput" placeholder="Search Users">
+            <div id="userList"></div>
           </div>
-          <div class="section" id="section3"><h3>Security Roles:</h3><ul></ul></div>
+          <div class="section" id="section2">
+            <h3>User Info 2</h3>
+            <input type="text" id="searchInput2" placeholder="Search Users">
+            <div id="userList2"></div>
+          </div>
+          <div id="sectionsRow">
+            <div class="section" id="section3">
+              <h3>Business Unit:</h3><ul id="businessUnitList"></ul>
+              <h3>Teams:</h3><ul id="teamsList"></ul>
+            </div>
+            <div class="section" id="section4"><h3>Security Roles:</h3><ul></ul></div>
+          </div>
         </div>
-      </div>
-    </div>`;
-}
+      </div>`;
+  }
   
 function createAndAppendPopup() {
     const popupHtml = createPopupHtml();
@@ -75,8 +77,8 @@ function createAndAppendPopup() {
     return popupDiv;
   }
 
-  function renderUserList(users, selectUserCallback) {
-    const userListDiv = document.getElementById('userList');
+  function renderUserList(users, selectUserCallback, userListDivId) {
+    const userListDiv = document.getElementById(userListDivId);
     users.forEach(user => {
       const userDiv = document.createElement('div');
       userDiv.className = 'user';
@@ -125,10 +127,10 @@ function createAndAppendPopup() {
     });
   }
 
-  function setupSearchFilter() {
-    document.getElementById('searchInput').oninput = function() {
+  function setupSearchFilter(inputId, userListClass) {
+    document.getElementById(inputId).oninput = function() {
       const searchValue = this.value.toLowerCase();
-      document.querySelectorAll('.user').forEach(el => {
+      document.querySelectorAll(userListClass).forEach(el => {
         el.style.display = el.textContent.toLowerCase().includes(searchValue) ? 'block' : 'none';
       });
     };
@@ -137,8 +139,10 @@ function createAndAppendPopup() {
   function displayPopup(users) {
     users.entities.sort((a, b) => a.fullname.localeCompare(b.fullname));
     createAndAppendPopup();
-    renderUserList(users.entities, selectUser);
-    setupSearchFilter();
+    renderUserList(users.entities, selectUser, 'userList');
+    renderUserList(users.entities, selectUser, 'userList2'); // User Info 2
+    setupSearchFilter('searchInput', '.user');
+    setupSearchFilter('searchInput2', '.user'); // User Info 2
   }
 
   fetchUsers(function(users) {
