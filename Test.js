98bb4ff1d0ece39e1d42808ad6647cfd896df3f6
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-25 13:02:06 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -78,7 +78,7 @@ javascript: (function() {
 
   function selectUser(user, sectionPrefix) {
   try {
-    document.querySelectorAll('.user').forEach(el => el.classList.remove('selected'));
+    document.querySelectorAll(`.user${sectionPrefix}`).forEach(el => el.classList.remove('selected'));
     const userDiv = document.getElementById('userList' + sectionPrefix).querySelector(`[data-id='${user.systemuserid}']`);
     userDiv.classList.add('selected');
 
