1a1bb9841c2c846ca22beb82d890fae65771e173
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-30 11:01:13 -0400

    Update fetchSecurityApi.js

--- fetchSecurityApi.js
+++ fetchSecurityApi.js
@@ -1,1 +1,15 @@
+function fetchUsers(callback) {
+	Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname,_businessunitid_value&$filter=(isdisabled eq false)').then(callback);
+}
 
+function fetchRolesForUser(userId, callback) {
+	Xrm.WebApi.retrieveMultipleRecords('systemuserroles', `?$filter=systemuserid eq ${userId}`).then(callback);
+}
+
+function fetchTeamsForUser(userId, callback) {
+	Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=teammembership_association($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+}
+
+function fetchBusinessUnitName(userId, callback) {
+	Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=businessunitid($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+}
