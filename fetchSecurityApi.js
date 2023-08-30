d3a0dc159e385429486a99d43bb169f0047f7349
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-30 15:56:22 -0400

    Update fetchSecurityApi.js

--- fetchSecurityApi.js
+++ fetchSecurityApi.js
@@ -1,35 +1,15 @@
 function fetchUsers(callback) {
-	var Xrm = window.Xrm || window.parent.Xrm;
-	if (Xrm) {
-		Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname,_businessunitid_value&$filter=(isdisabled eq false)').then(callback);
-	} else {
-		console.error("Xrm is not defined.");
-	}
-}
+    Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname,_businessunitid_value&$filter=(isdisabled eq false)').then(callback);
+  }
 
-function fetchRolesForUser(userId, callback) {
-	var Xrm = window.Xrm || window.parent.Xrm;
-	if (Xrm) {
-		Xrm.WebApi.retrieveMultipleRecords('systemuserroles', `?$filter=systemuserid eq ${userId}`).then(callback);
-	} else {
-		console.error("Xrm is not defined.");
-	}
-}
+  function fetchRolesForUser(userId, callback) {
+    Xrm.WebApi.retrieveMultipleRecords('systemuserroles', `?$filter=systemuserid eq ${userId}`).then(callback);
+  }
 
-function fetchTeamsForUser(userId, callback) {
-	var Xrm = window.Xrm || window.parent.Xrm;
-	if (Xrm) {
-		Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=teammembership_association($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
-	} else {
-		console.error("Xrm is not defined.");
-	}
-}
+  function fetchTeamsForUser(userId, callback) {
+    Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=teammembership_association($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+  }
 
-function fetchBusinessUnitName(userId, callback) {
-	var Xrm = window.Xrm || window.parent.Xrm;
-	if (Xrm) {
-		Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=businessunitid($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
-	} else {
-		console.error("Xrm is not defined.");
-	}
-}
+  function fetchBusinessUnitName(userId, callback) {
+    Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=businessunitid($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+ }
