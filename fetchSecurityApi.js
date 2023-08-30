c3330af9c2349900462d3117f2aa50b3f5bdea91
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-30 15:52:45 -0400

    Update fetchSecurityApi.js

--- fetchSecurityApi.js
+++ fetchSecurityApi.js
@@ -1,15 +1,35 @@
 function fetchUsers(callback) {
-	Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname,_businessunitid_value&$filter=(isdisabled eq false)').then(callback);
+	var Xrm = window.Xrm || window.parent.Xrm;
+	if (Xrm) {
+		Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname,_businessunitid_value&$filter=(isdisabled eq false)').then(callback);
+	} else {
+		console.error("Xrm is not defined.");
+	}
 }
 
 function fetchRolesForUser(userId, callback) {
-	Xrm.WebApi.retrieveMultipleRecords('systemuserroles', `?$filter=systemuserid eq ${userId}`).then(callback);
+	var Xrm = window.Xrm || window.parent.Xrm;
+	if (Xrm) {
+		Xrm.WebApi.retrieveMultipleRecords('systemuserroles', `?$filter=systemuserid eq ${userId}`).then(callback);
+	} else {
+		console.error("Xrm is not defined.");
+	}
 }
 
 function fetchTeamsForUser(userId, callback) {
-	Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=teammembership_association($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+	var Xrm = window.Xrm || window.parent.Xrm;
+	if (Xrm) {
+		Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=teammembership_association($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+	} else {
+		console.error("Xrm is not defined.");
+	}
 }
 
 function fetchBusinessUnitName(userId, callback) {
-	Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=businessunitid($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+	var Xrm = window.Xrm || window.parent.Xrm;
+	if (Xrm) {
+		Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=businessunitid($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+	} else {
+		console.error("Xrm is not defined.");
+	}
 }
