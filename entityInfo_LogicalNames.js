693a42e39f79a1c022b4b77838819450da843407
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-31 17:13:15 -0400

    Update entityInfo_LogicalNames.js

--- entityInfo_LogicalNames.js
+++ entityInfo_LogicalNames.js
@@ -1,56 +1,19 @@
 async function fetchEntityFields() {
-    console.log("fetchEntityFields called");
-    
-
-    const entityName = Xrm.Page.data.entity.getEntityName();
-    const recordId = Xrm.Page.data.entity.getId();
-    const cleanRecordId = recordId.replace(/[{}]/g, "");
-    const url = `${Xrm.Page.context.getClientUrl()}/api/data/v9.1/EntityDefinitions(LogicalName='${entityName}')/Attributes?$select=LogicalName,AttributeType,DisplayName`;
-
-    console.log(`Fetching from URL: ${url}`);
-
+    const {getEntityName, getId, context: {getClientUrl}} = Xrm.Page.data.entity;
+    const [entityName, recordId] = [getEntityName(), getId().replace(/[{}]/g, "")];
+    const url = `${getClientUrl()}/api/data/v9.1/EntityDefinitions(LogicalName='${entityName}')/Attributes?$select=LogicalName,AttributeType,DisplayName`;
     try {
         const response = await fetch(url);
-        if (response.ok) {
-            const results = await response.json();
-            const fieldListHtml = generateFieldListHtml(results.value);
-            const popupHtml = generatePopupHtml(entityName, cleanRecordId, fieldListHtml);
-            appendPopupToBody(popupHtml);
-        } else {
-            console.log(`Error: ${response.statusText}`);
-            alert(`Error: ${response.statusText}`);
-        }
-    } catch (error) {
-        console.log(`Error: ${error}`);
-        alert(`Error: ${error}`);
-    }
+        if (!response.ok) return alert(`Error: ${response.statusText}`);
+        const results = await response.json();
+        appendPopupToBody(generatePopupHtml(entityName, recordId, generateFieldListHtml(results.value)));
+    } catch (error) { alert(`Error: ${error}`); }
 }
 
-function generateFieldListHtml(fields) {
-    return fields
-        .filter(field => field.AttributeType !== 'Virtual' && field.DisplayName && field.DisplayName.UserLocalizedLabel && field.DisplayName.UserLocalizedLabel.Label)
-        .map((field, index) => `
-            <div>${index + 1}. <strong>${field.DisplayName.UserLocalizedLabel.Label}</strong>
-                <div style="margin-left: 20px;">
-                    <div>Name: ${field.LogicalName}</div>
-                    <div>Type: ${field.AttributeType}</div>
-                </div>
-            </div>
-        `)
-        .join('');
-}
+const generateFieldListHtml = fields => fields.filter(({AttributeType, DisplayName}) => AttributeType !== 'Virtual' && DisplayName?.UserLocalizedLabel?.Label)
+    .map(({DisplayName, LogicalName, AttributeType}, index) => `<div>${index + 1}. <strong>${DisplayName.UserLocalizedLabel.Label}</strong><div style="margin-left: 20px;"><div>Name: ${LogicalName}</div><div>Type: ${AttributeType}</div></div></div>`).join('');
 
-function generatePopupHtml(entityName, cleanRecordId, fieldListHtml) {
-    return `
-        <h2 style="text-align: left;">Entity: ${entityName}</h2>
-        <h2 style="text-align: left;">Record ID: ${cleanRecordId}</h2>
-        <h2 style="text-align: left;">Fields:</h2>
-        <br>
-        <div style="padding: 5px; columns: 2; -webkit-columns: 2; -moz-columns: 2;">
-            ${fieldListHtml}
-        </div>
-    `;
-}
+const generatePopupHtml = (entityName, cleanRecordId, fieldListHtml) => `<h2>Entity: ${entityName}</h2><h2>Record ID: ${cleanRecordId}</h2><h2>Fields:</h2><br><div style="padding: 5px; columns: 2;">${fieldListHtml}</div>`;
 const securityPopupCss = `
     .securityPopup { background-color: #f9f9f9; border: 3px solid #444; border-radius: 20px;width: 800px !important; height: 100% !important; overflow: hidden; box-shadow: 0 0 20px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; }
     .section { padding: 15px; border-right: 0px solid #ccc; overflow-y: scroll; }
@@ -61,41 +24,13 @@ const securityPopupCss = `
     .securityTooltipText { visibility: visible; width: 120px; background-color: black; color: #fff; text-align: center; border-radius: 6px; padding: 5px 0; position: absolute; z-index: 1; right: 100%; top: 50%; margin-top: -15px; opacity: 0; transition: opacity 0.3s; }
 `;
 
-function appendPopupToBody(html, clearPrevious = false) {
-	if (clearPrevious) {
-        	const existingPopups = document.querySelectorAll('.securityPopup');
-        	existingPopups.forEach(popup => popup.remove());
-    	}
-    // Creating the new container div
-	var newContainer = document.createElement('div');
-	
-	// Applying classes and styles to the new container
-	newContainer.className = 'securityPopup';	
-	newContainer.style.setProperty('width', '700px', 'important');
-   	newContainer.style.setProperty('height', '50%', 'important');
-	newContainer.style.position = 'fixed';  		
-	newContainer.style.top = '50%';
-	newContainer.style.left = '50%';
-	newContainer.style.transform = 'translate(-50%, -50%)'; 
-	
-	// Applying inner HTML to the new container
-	newContainer.innerHTML = `
-	    <div class="securityPopup-header">Copy User Security</div>
-	    <div id="securityTooltip" class="securityTooltip">
-	        i<span class="securityTooltipText" id="securityTooltiptext">
-	            This tool allows you to copy Business Unit, Teams, and Security Roles from one user to another.
-	        </span>
-	    </div>
-	    <style>${securityPopupCss}</style>
-	    <div class="securityPopup-row">
-	        <div class="section content-section" id="section1">
-	            ${html}
-	        </div>
-	    </div>
-	`;
-	
-	// Appending the new container to the document body
-	document.body.appendChild(newContainer);
+function appendPopupToBody(html) {
+    const newContainer = document.createElement('div');
+    newContainer.className = 'securityPopup';
+    newContainer.style.cssText = 'position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);';
+    newContainer.innerHTML = `<div class="securityPopup-header">Copy User Security</div><div id="securityTooltip" class="securityTooltip">i<span class="securityTooltipText">This tool allows you to copy Business Unit, Teams, and Security Roles from one user to another.</span></div><style>${securityPopupCss}</style><div class="securityPopup-row"><div class="section content-section">${html}</div></div>`;
+    
+    document.body.appendChild(newContainer);
 }
 
 
