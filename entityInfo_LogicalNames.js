393c47ecda9bb6284b1a9e5eb8af3306f92daf17
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-31 13:37:07 -0400

    Update entityInfo_LogicalNames.js

--- entityInfo_LogicalNames.js
+++ entityInfo_LogicalNames.js
@@ -25,28 +25,22 @@ function fetchEntityFields() {
                 
                 var html = '<h2 style="text-align: left;">Entity: ' + entityName + '</h2><h2 style="text-align: left;">Record ID: ' + cleanRecordId + '</h2><h2 style="text-align: left;">Fields:</h2><br><div style="padding: 5px; columns: 2; -webkit-columns: 2; -moz-columns: 2;">' + fieldList + '</div>';
 
-                var bookmarkletPopup = document.getElementById('bookmarkletPopup');
-
                 const securityPopupCss = `
-		    .securityPopup { background-color: #f9f9f9; border: 3px solid #444; border-radius: 20px; width: 800px; height: 100%; overflow: hidden; box-shadow: 0 0 20px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; }
-		    .section { padding: 20px; border-right: 0px solid #ccc; overflow-y: scroll; }
-		    .content-section { text-align: left; height: 500px; width: 100%; }
-		    .securityPopup-row { display: flex; }
-		    .securityPopup-header { text-align: center; padding: 10px; background-color: #444; color: #fff; font-size: 18px; border-bottom: 2px solid #333; border-radius: 20px 20px 0 0; }
-		    .securityTooltip { position: absolute; top: 15px; right: 15px; cursor: pointer; background-color: #fff; border: 1px solid #444; border-radius: 50%; width: 20px; height: 20px; text-align: center; font-size: 14px; line-height: 20px; }
-		    .securityTooltipText { visibility: hidden; width: 120px; background-color: black; color: #fff; text-align: center; border-radius: 6px; padding: 5px 0; position: absolute; z-index: 1; right: 100%; top: 50%; margin-top: -15px; opacity: 0; transition: opacity 0.3s; }
-		    .securityTooltip:hover .securityTooltipText { visibility: visible; opacity: 1; }
-		`;
+                    .securityPopup { background-color: #f9f9f9; border: 3px solid #444; border-radius: 20px; width: 800px; height: 100%; overflow: hidden; box-shadow: 0 0 20px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; }
+                    .section { padding: 20px; border-right: 0px solid #ccc; overflow-y: scroll; }
+                    .content-section { text-align: left; height: 500px; width: 100%; }
+                    .securityPopup-row { display: flex; }
+                    .securityPopup-header { text-align: center; padding: 10px; background-color: #444; color: #fff; font-size: 18px; border-bottom: 2px solid #333; border-radius: 20px 20px 0 0; }
+                    .securityTooltip { position: absolute; top: 15px; right: 15px; cursor: pointer; background-color: #fff; border: 1px solid #444; border-radius: 50%; width: 20px; height: 20px; text-align: center; font-size: 14px; line-height: 20px; }
+                    .securityTooltipText { visibility: hidden; width: 120px; background-color: black; color: #fff; text-align: center; border-radius: 6px; padding: 5px 0; position: absolute; z-index: 1; right: 100%; top: 50%; margin-top: -15px; opacity: 0; transition: opacity 0.3s; }
+                    .securityTooltip:hover .securityTooltipText { visibility: visible; opacity: 1; }
+                `;
 
-                if (bookmarkletPopup) {
-	            bookmarkletPopup.innerHTML = `<div class="securityPopup-header">Copy User Security</div><div id="securityTooltip" class="securityTooltip">i<span class="securityTooltipText" id="securityTooltiptext">This tool allows you to copy Business Unit, Teams, and Security Roles from one user to another.</span></div><style>${securityPopupCss}</style><div class="securityPopup-row"><div class="section content-section">${html}</div></div>`;
-	    	} else {
-	            var newContainer = document.createElement('div');
-	            newContainer.id = 'bookmarkletPopup';
-	            newContainer.className = 'securityPopup';
-	            newContainer.innerHTML = `<div class="securityPopup-header">Copy User Security</div><div id="securityTooltip" class="securityTooltip">i<span class="securityTooltipText" id="securityTooltiptext">This tool allows you to copy Business Unit, Teams, and Security Roles from one user to another.</span></div><style>${securityPopupCss}</style><div class="securityPopup-row"><div class="section content-section" id="section1">${html}</div></div>`;
-	            document.body.appendChild(newContainer);
-	        }
+                // Create and append the new container
+                var newContainer = document.createElement('div');
+                newContainer.className = 'securityPopup';
+                newContainer.innerHTML = `<div class="securityPopup-header">Copy User Security</div><div id="securityTooltip" class="securityTooltip">i<span class="securityTooltipText" id="securityTooltiptext">This tool allows you to copy Business Unit, Teams, and Security Roles from one user to another.</span></div><style>${securityPopupCss}</style><div class="securityPopup-row"><div class="section content-section">${html}</div></div>`;
+                document.body.appendChild(newContainer);
             } else {
                 alert("Error: " + this.statusText);
             }
@@ -54,6 +48,7 @@ function fetchEntityFields() {
     };
     xhr.send();
 }
+
 
 function renameTabsSectionsFields() { 
 	var currentFormId = Xrm.Page.ui.formSelector.getCurrentItem().getId();
