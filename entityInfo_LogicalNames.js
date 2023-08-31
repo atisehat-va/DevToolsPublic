ae810f9ec5b244b6d8724962fd5a7d38d36e1991
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-08-31 17:47:53 -0400

    Update entityInfo_LogicalNames.js

--- entityInfo_LogicalNames.js
+++ entityInfo_LogicalNames.js
@@ -18,6 +18,14 @@ async function fetchEntityFields() {
         alert(`Error: ${error}`);
     }
 }
+const securityPopupCss = `
+    .securityPopup { background-color: #f9f9f9; border: 3px solid #002050; border-radius: 20px;width: 800px !important; height: 100% !important; overflow: hidden; box-shadow: 0 0 20px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; }
+    .section { padding: 15px; border-right: 0px solid #ccc; overflow-y: scroll; }
+    .content-section { text-align: left; height: 78%; width: 100%; }
+    .securityPopup-row { display: flex; height: 100%; }
+    .securityPopup-header { text-align: center; padding: 10px; background-color: #002050; color: #fff; font-size: 18px; border-bottom: 2px solid #333; border-radius: 20px 20px 0 0; }
+    .scrollable-section { height: 400px; overflow-y: auto; }
+`;
 function generateFieldListHtml(fields) {
     return fields
         .filter(field => field.AttributeType !== 'Virtual' && field.DisplayName && field.DisplayName.UserLocalizedLabel && field.DisplayName.UserLocalizedLabel.Label)
@@ -42,14 +50,6 @@ function generatePopupHtml(entityName, cleanRecordId, fieldListHtml) {
         </div>
     `;
 }
-const securityPopupCss = `
-    .securityPopup { background-color: #f9f9f9; border: 3px solid #002050; border-radius: 20px;width: 800px !important; height: 100% !important; overflow: hidden; box-shadow: 0 0 20px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; }
-    .section { padding: 15px; border-right: 0px solid #ccc; overflow-y: scroll; }
-    .content-section { text-align: left; height: 78%; width: 100%; }
-    .securityPopup-row { display: flex; height: 100%; }
-    .securityPopup-header { text-align: center; padding: 10px; background-color: #002050; color: #fff; font-size: 18px; border-bottom: 2px solid #333; border-radius: 20px 20px 0 0; }
-    .scrollable-section { height: 400px; overflow-y: auto; }
-`;
 function appendPopupToBody(html, clearPrevious = false) {
 	if (clearPrevious) {
         	const existingPopups = document.querySelectorAll('.securityPopup');
