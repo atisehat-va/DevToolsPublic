bfc2fe7cac947f97e54d21742b96ea8e14b33a7b
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-01 14:58:44 -0400

    Update and rename fetchSecurityApi.js to fieldsControl.js

---
+++ fieldsControl.js
@@ -0,0 +1,55 @@
+function renameTabsSectionsFields() { 
+	var currentFormId = Xrm.Page.ui.formSelector.getCurrentItem().getId();
+	if (lastUpdatedFormId === currentFormId && logicalNameBtnClickStatus) {
+	   showContent('alert', 'Show Logical Names button has already been clicked!!');
+	   return;
+	}
+	Xrm.Page.ui.tabs.forEach(function(tab) {
+		var logicalName = tab.getName();
+		tab.setLabel(logicalName);
+		tab.sections.forEach(function(section) {
+			var logicalName = section.getName();
+			section.setLabel(logicalName);
+			section.controls.forEach(renameControlAndUpdateOptionSet);
+		});
+	});
+    logicalNameBtnClickStatus = true; 
+    lastUpdatedFormId = currentFormId;
+    renameHeaderFields();   
+}
+
+function renameHeaderFields() {
+    closeIframe();
+    var headerControls = Xrm.Page.ui.controls.get(function (control, index) {
+        var controlType = control.getControlType();
+        return controlType === "standard" || controlType === "optionset" || controlType === "lookup";
+    });
+    headerControls.forEach(renameControlAndUpdateOptionSet);   
+}
+
+
+function renameControlAndUpdateOptionSet(control) {
+	var attribute = control.getAttribute();
+	if (attribute !== null) {
+		var logicalName = attribute.getName();
+		control.setLabel(logicalName);
+		if (control.getControlType() === "optionset") {
+			updateOptionSetValues(control);            
+		}
+	}
+}
+
+function updateOptionSetValues(control) {	
+	var optionSetOptions = control.getOptions();
+	optionSetOptions.forEach(function(option) {
+		if (option.text !== "") {			
+			var originalText = option.text.split("(").pop().split(")")[0];			
+			var newText = option.value.toString() + " (" + originalText + ")";						
+			control.removeOption(option.value);
+			control.addOption({
+				value: option.value,
+				text: newText
+			}, option.value);
+		}
+	});    
+}
