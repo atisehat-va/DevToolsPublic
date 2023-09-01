35dcdf83bc884fa7a634067fbd67b82ba165a773
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-01 16:29:26 -0400

    Update copySecurityOriginal.js

--- copySecurityOriginal.js
+++ copySecurityOriginal.js
@@ -8,7 +8,7 @@ function securityUpdate() {
 	let selectedRoleIds = [];
 
 	const popupCss = `    
-		.securityPopup { background-color: #f9f9f9; border: 3px solid #444; border-radius: 20px; width: 800px; height: 100%; overflow: hidden; box-shadow: 0 0 20px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; }    
+		.securityPopup { background-color: #f9f9f9; border: 3px solid #002050; border-radius: 20px; width: 800px; height: 100%; overflow: hidden; box-shadow: 0 0 20px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; }    
 		.section { padding: 10px; border-right: 1px solid #ccc; overflow-y: scroll; }    
 		.section h3 { text-align: center; margin-bottom: 10px; color: #444; }    
 		.user-section { text-align: center; height: 190px; width: 50%;}    
@@ -26,8 +26,8 @@ function securityUpdate() {
 		#sectionsRow { white-space: nowrap; }    
 		.securityPopup-row { display: flex; }    
 		.securityPopup-header { text-align: center; padding: 10px; background-color: #444; color: #fff; font-size: 18px; border-bottom: 2px solid #333; border-radius: 20px 20px 0 0; }    .submit-button-container { text-align: center; padding: 10px; width: 100%; }      
-		#submitButton { display: none; margin: auto; padding: 10px 20px; font-size: 16px; width: 250px; background-color: #007bff; color: white; border: none; cursor: pointer; border-radius: 5px; transition: background-color 0.3s; }      
-		#submitButton:hover { background-color: #0056b3; }    
+		#submitButton { display: none; margin: auto; padding: 10px 20px; font-size: 16px; width: 250px; background-color: #333; color: white; border: none; cursor: pointer; border-radius: 5px; transition: background-color 0.3s; }      
+		#submitButton:hover { background-color: #444; }    
 		.tooltip { position: absolute; top: 15px; right: 15px; cursor: pointer; background-color: #fff; border: 1px solid #444; border-radius: 50%; width: 20px; height: 20px; text-align: center; font-size: 14px; line-height: 20px; }    
 		.tooltiptext { visibility: visible; width: 120px; background-color: black; color: #fff; text-align: center; border-radius: 6px; padding: 5px 0; position: absolute; z-index: 1; right: 100%; top: 50%; margin-top: -15px; opacity: 0; transition: opacity 0.3s; }    
 		.tooltip:hover .tooltiptext { visibility: visible; opacity: 1; }  
