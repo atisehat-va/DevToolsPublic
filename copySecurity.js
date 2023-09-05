663a2be12ff6cba39e75899823fa7d44abd6bd62
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-05 12:05:40 -0400

    Update copySecurity.js

--- copySecurity.js
+++ copySecurity.js
@@ -87,6 +87,7 @@ function securityUpdate() {
 		    openPopup();  
 		});
 		return newContainer;
+		makePopupMovable(newContainer);
 	}
 
 	function renderUserList(users, selectUserCallback, sectionId, searchInputId) {
@@ -316,4 +317,34 @@ function securityUpdate() {
 		script.src = src;
 		document.body.appendChild(script);
 	}
+}
+
+function makePopupMovable(newContainer) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  newContainer.onmousedown = dragMouseDown;
+
+  function dragMouseDown(e) {
+    e = e || window.event;    
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    newContainer.style.top = (newContainer.offsetTop - pos2) + "px";
+    newContainer.style.left = (newContainer.offsetLeft - pos1) + "px";
+  }
+
+    function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
