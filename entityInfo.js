964755a1028477b5fd5a8109d8b1a2c0d3d9da63
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-05 11:14:31 -0400

    Update entityInfo.js

--- entityInfo.js
+++ entityInfo.js
@@ -18,15 +18,7 @@ async function fetchEntityFields() {
         alert(`Error: ${error}`);
     }
 }
-const entityInfoPopupCss = `
-    .entityInfoPopup { background-color: #f9f9f9; border: 3px solid #002050; border-radius: 20px; width: 40%; height: 50%; overflow: hidden; box-shadow: 0 0 20px rgba(0, 0, 0, 0.5); font-family: Arial, sans-serif; }
-    .section { padding: 15px; border-right: 0px solid #ccc; overflow-y: scroll; }
-    .content-section { text-align: left; height: 100%; width: 100%; }
-    .entityInfoPopup-row { display: flex; height: 100%; }
-    .entityInfoPopup-header { position: relative; text-align: center; font-size: 18px; padding: 10px; background-color: #002050; color: #fff; border: none;}
-    .scrollable-section { height: 66%; overflow-y: auto; }
-    .back-button { position: absolute; top: 0; left: 0; width: 90px; cursor: pointer; background-color: #333; color: #fff; padding: 10px; border-top-left-radius: 20px; border-bottom-right-radius: 15px; }
-`;
+
 function generateFieldListHtml(fields) {
     return fields
         .filter(field => field.AttributeType !== 'Virtual' && field.DisplayName && field.DisplayName.UserLocalizedLabel && field.DisplayName.UserLocalizedLabel.Label)
@@ -57,18 +49,12 @@ function appendPopupToBody(html, clearPrevious = false) {
         	existingPopups.forEach(popup => popup.remove());
     	}    
 	var newContainer = document.createElement('div');	  	
-	newContainer.className = 'entityInfoPopup';	
-	newContainer.style.position = 'fixed';  		
-	newContainer.style.top = '50%';
-	newContainer.style.left = '50%';
-	newContainer.style.transform = 'translate(-50%, -50%)'; 
-		
+	newContainer.className = 'entityInfoPopup';		
 	newContainer.innerHTML = `
 	    <div class="entityInfoPopup-header">
                 <button class="back-button" id="back-button">Back</button>
      		Entity & Fields Info
-            </div>	    
-	    <style>${entityInfoPopupCss}</style>
+            </div>   
 	    <div class="entityInfoPopup-row">
 	        <div class="section content-section" id="section1">
 	            ${html}
