bd77adb52147e80e2e1d50ee079664974f6f88d6
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-05 11:06:21 -0400

    Update showDirtyFields.js

--- showDirtyFields.js
+++ showDirtyFields.js
@@ -1,31 +1,3 @@
-/*function showDirtyFields() {
-    var entity = Xrm.Page.data.entity;
-    var attributes = entity.attributes.get();
-    var dirtyFields = attributes.filter(function(attribute) {
-        return attribute.getIsDirty();
-    });
-
-    var html = '<h2 style="text-align: center;"><strong>Dirty Fields</strong></h2>';
-
-    if (dirtyFields.length > 0) {
-        var fieldList = dirtyFields.map(function(attribute, index) {
-            var logicalName = attribute.getName();
-            var control = attribute.controls.get(0);
-            var displayName = control ? control.getLabel() : logicalName;
-
-            return '<div>' + (index + 1) + '. <strong>' + displayName + '</strong></div>' +
-                   '<div style="margin-left: 40px;">&bull; <strong>Logical Name:</strong> ' + logicalName + '</div><br>';
-        }).join('');
-
-        html += '<div style="padding: 5px;">' + fieldList + '</div>';
-    } else {
-        html += '<div>No dirty fields found.</div>';
-    }
-
-    // Calling the showContent function with 'html' type
-    showContent('html', html);
-}*/
-
 function generateDirtyFieldsHtml(dirtyFields) {
     if (dirtyFields.length > 0) {
         return dirtyFields.map((attribute, index) => {
