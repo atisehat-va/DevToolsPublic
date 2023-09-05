a3964fb9ffd2f7521124633df84eb1d068a82c0b
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-05 10:06:10 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -98,13 +98,7 @@ function openPopup() {
   `;
   var popupDiv = document.createElement('div');
   popupDiv.id = 'MenuPopup';
-  popupDiv.innerHTML = popupHtml;
-  popupDiv.style.position = 'absolute';
-  popupDiv.style.zIndex = '10000';
-  popupDiv.style.left = '50%';
-  popupDiv.style.top = '50%';
-  popupDiv.style.transform = 'translate(-50%, -50%)';
-  popupDiv.style.backgroundColor = 'white';  
+  popupDiv.innerHTML = popupHtml;   
   document.body.appendChild(popupDiv);
   
   makePopupMovable(popupDiv);
