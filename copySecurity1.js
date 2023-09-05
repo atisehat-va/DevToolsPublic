808bc15ad26b98f61d6a1a91d877508faa7230b5
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-05 19:48:07 -0400

    Update copySecurity1.js

--- copySecurity1.js
+++ copySecurity1.js
@@ -316,32 +316,38 @@ function securityUpdate() {
 	}
 
 	function makePopupMovable(newContainer) {
-	  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-	  newContainer.onmousedown = dragMouseDown;
+	    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	    newContainer.onmousedown = function(e) {
+		if (e.target.tagName.toLowerCase() === "input") {
+		    // If it's an input element, do nothing and return
+		    return;
+		}
+		dragMouseDown(e);
+	    };
 	
-	  function dragMouseDown(e) {
-	    e = e || window.event;    
-	    e.preventDefault();
-	    pos3 = e.clientX;
-	    pos4 = e.clientY;
-	    document.onmouseup = closeDragElement;
-	    document.onmousemove = elementDrag;
-	  }
+	    function dragMouseDown(e) {
+		e = e || window.event;
+		e.preventDefault();
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		document.onmousemove = elementDrag;
+	    }
 	
-	  function elementDrag(e) {
-	    e = e || window.event;
-	    e.preventDefault();
-	    pos1 = pos3 - e.clientX;
-	    pos2 = pos4 - e.clientY;
-	    pos3 = e.clientX;
-	    pos4 = e.clientY;
-	    newContainer.style.top = (newContainer.offsetTop - pos2) + "px";
-	    newContainer.style.left = (newContainer.offsetLeft - pos1) + "px";
-	  }
+	    function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		newContainer.style.top = (newContainer.offsetTop - pos2) + "px";
+		newContainer.style.left = (newContainer.offsetLeft - pos1) + "px";
+	    }
 	
 	    function closeDragElement() {
-	    document.onmouseup = null;
-	    document.onmousemove = null;
-	  }
-	}
+		document.onmouseup = null;
+		document.onmousemove = null;
+	    }
+}
 }
