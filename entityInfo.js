d88aac37ea7f9961a16fe923cdd17115b0728a94
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-05 20:41:08 -0400

    Update entityInfo.js

--- entityInfo.js
+++ entityInfo.js
@@ -68,33 +68,3 @@ function appendPopupToBody(html, clearPrevious = false) {
 	});
 	makePopupMovable(newContainer);
 } 
-/*
-function makePopupMovable(newContainer) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  newContainer.onmousedown = dragMouseDown;
-
-  function dragMouseDown(e) {
-    e = e || window.event;    
-    e.preventDefault();
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    newContainer.style.top = (newContainer.offsetTop - pos2) + "px";
-    newContainer.style.left = (newContainer.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}*/
