b1ac21c8d3b0e81dc86e5ba3778ad66adf00037a
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-09 16:31:26 -0400

    Update assignUserSecurity.js

--- assignUserSecurity.js
+++ assignUserSecurity.js
@@ -96,7 +96,7 @@ function securityUpdate2() {
 	        </div>
 	      </div>
 	      <div class="assignSection rightTeam-section" id="section6">
-	        <h3>Add/Remove Security Role(s):</h3>
+	        <h3>Add | Remove Security Role(s):</h3>
 	        <div class="teamsRoles-input-wrapper">
 	           <input type="text" id="searchInput4" placeholder="Search Security Role">	 
 	        </div>
