ed72cd1b8cc4c2fb06614e731470066cbc908d25
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-09 15:12:03 -0400

    Update assignUserSecurity.js

--- assignUserSecurity.js
+++ assignUserSecurity.js
@@ -42,7 +42,7 @@ function securityUpdate2() {
 	        </div>
 	      </div>                            
 	      <div class="assignSection rightBuss-section" id="section2">
-	        <h3>Change Business Unit</h3>
+	        <h3>Change Business Unit:</h3>
 	        <input type="text" id="searchInput2" placeholder="Search Business Units">
 	        <div class="businessUnit-list-container">
 	          <div id="businessUnitList"></div>
