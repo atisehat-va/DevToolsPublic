1be58766c47c2629df0f7c3f8c022e529beda22d
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-11 17:24:07 -0400

    Update assignUserSecurity.js

--- assignUserSecurity.js
+++ assignUserSecurity.js
@@ -355,7 +355,32 @@ function securityUpdate2() {
 					   });
 					});
 				});
-
+				// Function to display roles
+				const displayRoles = (rolesArr, targetElement) => {
+				    targetElement.innerHTML = '';
+				    rolesArr.forEach(roleDetail => {
+				        // Create checkbox
+				        const assignCheckbox = document.createElement('input');
+				        assignCheckbox.type = 'checkbox';
+				        assignCheckbox.value = roleDetail.roleid; // set value to role's ID
+				        assignCheckbox.className = 'assignCheckbox'; // for styling or selection
+				        
+				        // Create label for readability
+				        const label = document.createElement('label');
+				        label.innerHTML = roleDetail.name;
+				        label.insertBefore(assignCheckbox, label.firstChild);
+				
+				        // Create wrapper div
+				        const wrapperDiv = document.createElement('div');
+				        wrapperDiv.className = 'sectionWrapper';
+				
+				        // Append checkbox and label to wrapper div
+				        wrapperDiv.appendChild(label);
+				
+				        // Append wrapper div to the roles list
+				        targetElement.appendChild(wrapperDiv);
+				    });
+				};
 				// Fetch roles based on the business unit and display them under section6
 				const rolesListBusinessUnit = document.getElementById('section6').querySelector('#securityRolesList');
 				rolesListBusinessUnit.innerHTML = '';
