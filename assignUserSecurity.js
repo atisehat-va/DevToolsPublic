b5074bf74c2591316a452587695943828932a259
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-21 17:08:40 -0400

    Update assignUserSecurity.js

--- assignUserSecurity.js
+++ assignUserSecurity.js
@@ -488,8 +488,8 @@ function securityUpdate2() {
 				        const messageDiv = createAndAppendMessageDiv(event.target.parentNode, 'Your update is in progress, please be patient...', 'updateMessage');
 				
 				        if (typeof updateUserDetails === "function") {				            					    
-					    await handleConditions(businessUnitRadioSelected, teamsRadioSelected, teamsCheckedValues, rolesRadioSelected, rolesCheckedValues);
 					    toggleCheckboxes('disable', ['assignCheckbox', 'teamsCheckbox', 'teamsRadioButtons', 'rolesCheckbox', 'rolesRadioButtons', 'businessUnitRadioButtons']);
+					    await handleConditions(businessUnitRadioSelected, teamsRadioSelected, teamsCheckedValues, rolesRadioSelected, rolesCheckedValues);					    
 					    toggleCheckboxes('enable', ['teamsRadioButtons', 'rolesRadioButtons', 'businessUnitRadioButtons']);
 					    teamsCheckedValues = [];
 					    rolesCheckedValues = [];
@@ -500,7 +500,7 @@ function securityUpdate2() {
 				            // Remove message and show update
 				            removeElementById('updateMessage');
 				            createAndAppendMessageDiv(event.target.parentNode, `Security updated for ${selectedUserId}`, 'updateMessage');
-					    
+					    selectUser(user, sectionPrefix);					    
 				        } else {
 				            console.log("updateUserDetails is NOT accessible");
 				        }
