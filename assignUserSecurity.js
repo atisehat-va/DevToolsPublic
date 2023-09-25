66751e10b8a32ed7e5058fee7e00bbb39438d1bb
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-25 19:05:00 -0400

    Update assignUserSecurity.js

--- assignUserSecurity.js
+++ assignUserSecurity.js
@@ -599,7 +599,7 @@ function securityUpdate2() {
 		    }
 	     } else {
 		initSubmitButton();
-		showCustomAlert('Nothing was selected to change.');
+		showCustomAlert('To update user security, please select from one of the following categories: Business Unit, Team, or Security Role.');
 		console.log('Nothing was selected to change.');
 	    }
 	}
