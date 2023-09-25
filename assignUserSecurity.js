480b488ea3df4c6142b1ad827121d79f8002f10b
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-25 11:36:40 -0400

    Update assignUserSecurity.js

--- assignUserSecurity.js
+++ assignUserSecurity.js
@@ -51,7 +51,7 @@ function securityUpdate2() {
 	        </div>
 	      </div> 
 	      <div class="assignSection rightBuss-section" id="section2">
-       		<h3 id="bUh3">To modify user security settings, please choose a user from the list.</h3>
+       		<h3 id="bUh3" style="display: none;" >Change Business Unit:</h3>
 	        <input type="text" id="searchInput2" placeholder="Search Business Units" style="display: none;">
 	        <div class="businessUnit-list-container">
 	          <div id="businessUnitList"></div>
@@ -66,7 +66,8 @@ function securityUpdate2() {
 	        </div>
 	      </div>
 	      <div class="assignSection rightTeam-section" id="section5">	        
-	     	 <div class="teams-wrapper">		  
+	     	 <div class="teams-wrapper">
+		 <h3 id="teamsH3" style="display: block;" >To modify user security settings, please choose a user from the list.</h3>
 	          <div class="teamsRoles-list-container">	          
 		  <div id="teamsList"></div>		   
 		</div>	  
@@ -100,23 +101,16 @@ function securityUpdate2() {
 	  });		
 	  makePopupMovable(newContainer);	
 	}
-	
-	function toggleChangeBuInputAndHeading(newHeaderText) {
-	  const h3Element = document.getElementById('bUh3');
-	  const inputElement = document.getElementById('searchInput2');
-	
-	  if (h3Element && inputElement) {
-	    // Update the text of the h3 element
-	    h3Element.textContent = newHeaderText;
-	
-	    // Toggle the visibility of the input element
-	    if (inputElement.style.display === 'none') {
-	      inputElement.style.display = 'block';
-	    } else {
-	      inputElement.style.display = 'none';
+
+	function toggleChangeBuInputAndHeading() {
+	  const idsToToggle = ['bUh3', 'searchInput2', 'teamsH3'];	
+	  idsToToggle.forEach(elementId => {
+	    const element = document.getElementById(elementId);
+	    if (element) {
+	      element.style.display = element.style.display === 'none' ? 'block' : 'none';
 	    }
-	  }
-	}
+	  });
+	}	
 	
 	function toggleCheckboxes(action, classNames) {
 	  // Accept either a single class name or an array of class names
@@ -438,7 +432,7 @@ function securityUpdate2() {
 				});
 				
 				//newStuff
-				toggleChangeBuInputAndHeading('Change Business Unit:');      			
+				//toggleChangeBuInputAndHeading();      			
 				if (businessUnits && businessUnits.entities) {
 				    renderGenericList(businessUnits.entities, businessUnit => selectItem(businessUnit, '1'), 'businessUnitList', 'searchInput2', 'businessUnit', 'name', 'id');		
 				}
