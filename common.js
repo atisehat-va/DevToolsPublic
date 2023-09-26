9898e1d56e40c674f6fa834c90dffd9ed6ef90d4
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-26 15:25:25 -0400

    Update common.js

--- common.js
+++ common.js
@@ -1,3 +1,27 @@
+//CommonFetch
+function fetchUsers(callback) {
+    Xrm.WebApi.retrieveMultipleRecords('systemuser', '?$select=systemuserid,fullname,_businessunitid_value&$filter=(isdisabled eq false)').then(callback);
+}
+function fetchRolesForUser(userId, callback) {
+	Xrm.WebApi.retrieveMultipleRecords('systemuserroles', `?$filter=systemuserid eq ${userId}`).then(callback);
+}
+function fetchTeamsForUser(userId, callback) {
+	Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=teammembership_association($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+}
+function fetchBusinessUnitName(userId, callback) {
+	Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=businessunitid($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+}
+function fetchBusinessUnits(callback) {
+	Xrm.WebApi.retrieveMultipleRecords('businessunit', '?$select=businessunitid,name').then(callback);
+}
+function fetchTeams(callback) {
+	Xrm.WebApi.retrieveMultipleRecords('team', '?$select=teamid,name&$expand=businessunitid($select=name)').then(callback);
+}
+function fetchSecurityRoles(businessUnitId, callback) {
+	Xrm.WebApi.retrieveMultipleRecords('role', `?$select=roleid,name&$filter=_businessunitid_value eq ${businessUnitId}`).then(callback);
+}
+
+
 //Make window movable
 function makePopupMovable(newContainer) {
    console.log("MakePopupMovable");
