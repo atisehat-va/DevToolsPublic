c44b601f1984e2a7865acb9601a0c0b2b7d60410
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-28 13:42:51 -0400

    Update userProvisionOLD.js

--- userProvisionOLD.js
+++ userProvisionOLD.js
@@ -114,36 +114,91 @@ function calculateAdjustedDate(executionContext) {
         console.error(error.message);
     });
 }
+//newTest
+function calculateAdjustedDate(executionContext) {
+    var formContext = executionContext.getFormContext();
 
-function getHolidaysForSchedule() {
-    const fetchXml = `
-        <fetch>
-            <entity name="calendar">
-                <filter>
-                    <condition attribute="name" operator="eq" value="Federal Holiday Schedule" />
-                </filter>
-                <link-entity name="calendarrule" from="calendarid" to="calendarid" alias="rule">
-                    <attribute name="name" />
-                    <attribute name="starttime" />
-                    <filter>
-                        <condition attribute="starttime" operator="this-year" />
-                    </filter>
-                </link-entity>
-            </entity>
-        </fetch>
-    `;
-    Xrm.WebApi.retrieveMultipleRecords("calendar", `?fetchXml=${encodeURIComponent(fetchXml)}`).then(
-        results => {
-            for (const result of results.entities) {
-                const holidayName = result["rule.name"];
-                const holidayDate = result["rule.starttime"];
-                console.log("Holiday Name:", holidayName);
-                console.log("Holiday Date:", holidayDate);
-            }
-        },
-        error => {
-            console.log(error.message);
+    function getCaseDate(caseId) {
+        return Xrm.WebApi.online.retrieveRecord("Case", caseId, "?$select=new_caseDate");
+    }
+
+    function getHolidaysForSchedule() {
+        return new Promise((resolve, reject) => {
+            const fetchXml = `
+                <fetch>
+                    <entity name="calendar">
+                        <filter>
+                            <condition attribute="name" operator="eq" value="Federal Holiday Schedule" />
+                        </filter>
+                        <link-entity name="calendarrule" from="calendarid" to="calendarid" alias="rule">
+                            <attribute name="name" />
+                            <attribute name="starttime" />
+                            <filter>
+                                <condition attribute="starttime" operator="this-year" />
+                            </filter>
+                        </link-entity>
+                    </entity>
+                </fetch>
+            `;
+            Xrm.WebApi.retrieveMultipleRecords("calendar", `?fetchXml=${encodeURIComponent(fetchXml)}`).then(
+                results => {
+                    let holidays = results.entities.map(entity => new Date(entity["rule.starttime"]));
+                    resolve(holidays);
+                },
+                error => {
+                    reject(error.message);
+                }
+            );
+        });
+    }
+
+    function isWeekend(date) {
+        var day = date.getUTCDay();
+        return day === 0 || day === 6;
+    }
+
+    // Retrieve reference to Case from the current form (assuming a lookup/reference field)
+    var caseReference = formContext.getAttribute("your_case_lookup_field_name").getValue();
+    if (!caseReference || caseReference.length === 0) {
+        console.error("Case reference not found.");
+        return;
+    }
+    var caseId = caseReference[0].id;
+
+    getCaseDate(caseId)
+    .then(function(caseResult) {
+        var caseDate = new Date(caseResult.new_caseDate);
+
+        // Get the SLA number
+        return Xrm.WebApi.online.retrieveMultipleRecords("SLA", "?$top=1&$select=new_slaNumber");
+    })
+    .then(function(slaResults) {
+        if (slaResults.entities.length) {
+            var slaNumber = slaResults.entities[0].new_slaNumber;
+
+            // Get the list of holidays
+            return getHolidaysForSchedule();
+        } else {
+            throw new Error("No SLA record found.");
         }
-    );
+    })
+    .then(function(holidays) {
+        var addedDays = 0;
+        var caseDate = new Date(formContext.getAttribute("new_caseDate").getValue());
+        while (addedDays < slaNumber) {
+            caseDate.setDate(caseDate.getDate() + 1);
+
+            if (isWeekend(caseDate) || holidays.includes(caseDate.toISOString().split("T")[0])) {
+                continue;
+            }
+
+            addedDays++;
+        }
+
+        var adjustedDate = caseDate;
+        console.log("Adjusted Date:", adjustedDate); // Print the adjusted date
+    })
+    .catch(function(error) {
+        console.error(error.message);
+    });
 }
-getHolidaysForSchedule();
