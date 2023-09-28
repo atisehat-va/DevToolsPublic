4731400d69fb33920c7f515b5c9e765cbea0c1bd
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-28 16:41:49 -0400

    Update showHiddenItems_UnlockFields.js

--- showHiddenItems_UnlockFields.js
+++ showHiddenItems_UnlockFields.js
@@ -10,41 +10,39 @@ function unlockAllFields() {
 	showCustomAlert('Unlock All Fields button has already been clicked!!');	
         //return;
     }
-	var allControls = Xrm.Page.ui.controls.get();
-	for (var i in allControls) {
-		var control = allControls[i];
-		if (control) {
-			control.setDisabled(false);
-		}
+    var allControls = Xrm.Page.ui.controls.get();
+    for (var i in allControls) {
+	var control = allControls[i];
+	if (control) {
+		control.setDisabled(false);
 	}
+    }
     unlockAllFieldsBtnClickStatus = true;
     lastUpdatedFormId = currentFormId;
 }
 
 function showAllTabsAndSections() {
-    closeIframe();
-	debugger;
+    closeIframe();	
     var currentFormId = Xrm.Page.ui.formSelector.getCurrentItem().getId();
     if (lastUpdatedFormId === currentFormId && showAllTabsAndSectionsBtnClickStatus) {
 	showCustomAlert('Show Hidden Items button has already been clicked!!');		
         //return;
     }
-	Xrm.Page.ui.tabs.forEach(function(tab) {
-		if (!tab.getVisible()) {
-			tab.setVisible(true);			
+    Xrm.Page.ui.tabs.forEach(function(tab) {
+	if (!tab.getVisible()) {
+		tab.setVisible(true);			
+	}
+	tab.sections.forEach(function(section) {
+		if (!section.getVisible()) {
+			section.setVisible(true);
 		}
-		tab.sections.forEach(function(section) {
-			if (!section.getVisible()) {
-				section.setVisible(true);
+		section.controls.forEach(function(control) {
+			if (!control.getVisible()) {
+				control.setVisible(true);
 			}
-			section.controls.forEach(function(control) {
-				if (!control.getVisible()) {
-					control.setVisible(true);
-				}
-			});
-		});		
-	});    
-    showAllTabsAndSectionsBtnClickStatus = true;
-    lastUpdatedFormId = currentFormId;
-    
+		});
+	});		
+    });    
+   showAllTabsAndSectionsBtnClickStatus = true;
+   lastUpdatedFormId = currentFormId;    
 }
