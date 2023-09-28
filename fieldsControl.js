6b1c259877408e87b3e79048a8f61ef3d031556a
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-28 16:55:01 -0400

    Update fieldsControl.js

--- fieldsControl.js
+++ fieldsControl.js
@@ -3,57 +3,61 @@ var logicalNameBtnClickStatus = false;
 var unlockAllFieldsBtnClickStatus = false;
 var showAllTabsAndSectionsBtnClickStatus = false;
 
-function renameTabsSectionsFields() { 
-	var currentFormId = Xrm.Page.ui.formSelector.getCurrentItem().getId();
-	if (lastUpdatedFormId === currentFormId && logicalNameBtnClickStatus) {
-	   showCustomAlert('Show Logical Names button has already been clicked!!');	   
-	   return;
-	}
-	Xrm.Page.ui.tabs.forEach(function(tab) {
-		var logicalName = tab.getName();
-		tab.setLabel(logicalName);
-		tab.sections.forEach(function(section) {
-			var logicalName = section.getName();
-			section.setLabel(logicalName);
-			section.controls.forEach(renameControlAndUpdateOptionSet);
-		});
-	});
+function renameTabsSectionsFields(formContext) { 
+    var currentFormId = formContext.ui.formSelector.getCurrentItem().getId();
+    if (lastUpdatedFormId === currentFormId && logicalNameBtnClickStatus) {
+        showCustomAlert('Show Logical Names button has already been clicked!!');	   
+        return;
+    }
+    formContext.ui.tabs.forEach(function(tab) {
+        var logicalName = tab.getName();
+        tab.setLabel(logicalName);
+        tab.sections.forEach(function(section) {
+            var logicalName = section.getName();
+            section.setLabel(logicalName);
+            section.controls.forEach(function(control) {
+                renameControlAndUpdateOptionSet(control, formContext);
+            });
+        });
+    });
     logicalNameBtnClickStatus = true; 
     lastUpdatedFormId = currentFormId;
-    renameHeaderFields();   
+    renameHeaderFields(formContext);   
 }
 
-function renameHeaderFields() {
+function renameHeaderFields(formContext) {
     closeIframe();
-    var headerControls = Xrm.Page.ui.controls.get(function (control, index) {
+    var headerControls = formContext.ui.controls.get(function (control, index) {
         var controlType = control.getControlType();
         return controlType === "standard" || controlType === "optionset" || controlType === "lookup";
     });
-    headerControls.forEach(renameControlAndUpdateOptionSet);   
+    headerControls.forEach(function(control) {
+        renameControlAndUpdateOptionSet(control, formContext);
+    });   
 }
 
-function renameControlAndUpdateOptionSet(control) {
-	var attribute = control.getAttribute();
-	if (attribute !== null) {
-		var logicalName = attribute.getName();
-		control.setLabel(logicalName);
-		if (control.getControlType() === "optionset") {
-			updateOptionSetValues(control);            
-		}
-	}
+function renameControlAndUpdateOptionSet(control, formContext) {
+    var attribute = control.getAttribute();
+    if (attribute !== null) {
+        var logicalName = attribute.getName();
+        control.setLabel(logicalName);
+        if (control.getControlType() === "optionset") {
+            updateOptionSetValues(control);            
+        }
+    }
 }
 
 function updateOptionSetValues(control) {	
-	var optionSetOptions = control.getOptions();
-	optionSetOptions.forEach(function(option) {
-		if (option.text !== "") {			
-			var originalText = option.text.split("(").pop().split(")")[0];			
-			var newText = option.value.toString() + " (" + originalText + ")";						
-			control.removeOption(option.value);
-			control.addOption({
-				value: option.value,
-				text: newText
-			}, option.value);
-		}
-	});    
+    var optionSetOptions = control.getOptions();
+    optionSetOptions.forEach(function(option) {
+        if (option.text !== "") {			
+            var originalText = option.text.split("(").pop().split(")")[0];			
+            var newText = option.value.toString() + " (" + originalText + ")";						
+            control.removeOption(option.value);
+            control.addOption({
+                value: option.value,
+                text: newText
+            }, option.value);
+        }
+    });    
 }
