24a08a13e94ad31a87fe832d36f51bdb55a16d58
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-29 18:33:28 -0400

    Update dateCalc.js

--- dateCalc.js
+++ dateCalc.js
@@ -1,38 +1,52 @@
 function dateCalc() {
     var newContainer = document.createElement('div');
     newContainer.className = 'commonPopup';
-    newContainer.style.display = 'flex'; // Setting the container to flex layout
-    newContainer.style.flexDirection = 'column'; // Stacking children vertically
+    newContainer.style.display = 'flex';
+    newContainer.style.flexDirection = 'column';
     newContainer.innerHTML = `
         <div class="commonPopup-header">
             <button class="commonback-button" id="commonback-button">Back</button>
             Date Calculator
-        </div>        
-        <div class="grid-container">
-            <div class="grid-item" id="section-1"></div>
-            <div class="grid-item" id="section-2"></div>
-            <div class="grid-item" id="section-3"></div>
-            <div class="grid-item" id="section-4"></div>
-            <div class="grid-item" id="section-5"></div>
-            <div class="grid-item" id="section-6"></div>
+        </div>
+        <div class="sections-container">
+            <div class="section-side">
+                <div class="section" id="section-1"></div>
+                <div class="section" id="section-2"></div>
+                <div class="section" id="section-3"></div>
+            </div>
+            <div class="vertical-divider"></div>
+            <div class="section-side">
+                <div class="section" id="section-4"></div>
+                <div class="section" id="section-5"></div>
+                <div class="section" id="section-6"></div>
+            </div>
         </div>
         <style>
-            .grid-container {
-                display: grid;
-                grid-template-columns: repeat(2, 1fr);
-                grid-template-rows: repeat(3, 1fr);
-                gap: 15px;
-                padding: 20px;
-                flex-grow: 1; // This makes the grid container take up the remaining space
+            .sections-container {
+                display: flex;
+                flex-grow: 1;
+                align-items: stretch;
             }
-            .grid-item {
+
+            .section-side {
+                flex: 1;
+                display: flex;
+                flex-direction: column;
+            }
+
+            .section {
+                flex: 1;
                 padding: 20px;
                 background-color: #f1f1f1;
-                display: flex;
-                justify-content: center;
-                align-items: center;
                 border-radius: 8px;
                 box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+                margin: 5px;
+            }
+
+            .vertical-divider {
+                width: 2px;
+                background-color: #000;
+                margin: 0 5px;
             }
         </style>
     `;
