6a7885d7f4583ee8ade82049b3757cf569ae16ff
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-29 17:52:07 -0400

    Update dateCalc.js

--- dateCalc.js
+++ dateCalc.js
@@ -1,3 +1,21 @@
+function generateDirtyFieldsHtml(dirtyFields) {
+    if (dirtyFields.length > 0) {
+        return dirtyFields.map((attribute, index) => {
+            const logicalName = attribute.getName();
+            const control = attribute.controls.get(0);
+            const displayName = control ? control.getLabel() : logicalName;
+            return `
+                <div>${index + 1}. <strong>${displayName}</strong>
+                    <div style="margin-left: 40px;">&bull; <strong>Logical Name:</strong> ${logicalName}</div>
+                </div>
+                <br>
+            `;
+        }).join('');
+    } else {
+        return '<div>No dirty fields found.</div>';
+    }
+}
+
 function appendDirtyFieldsPopupToBody(html) {
     var newContainer = document.createElement('div');
     newContainer.className = 'commonPopup';
@@ -19,7 +37,12 @@ function appendDirtyFieldsPopupToBody(html) {
 }
 
 function dateCalc() {       
-    const popupHtml = `        
+    const entity = Xrm.Page.data.entity;
+    const attributes = entity.attributes.get();
+    const dirtyFields = attributes.filter(attribute => attribute.getIsDirty());
+    const dirtyFieldsHtml = generateDirtyFieldsHtml(dirtyFields);
+    const popupHtml = `
+        <h2 style="text-align: left;"><strong>Dirty Fields:</strong></h2>
         <div class="scrollable-section" style="padding: 10px; columns: 2; -webkit-columns: 2; -moz-columns: 2;">
             ${dirtyFieldsHtml}
         </div>
