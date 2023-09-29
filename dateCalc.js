9f5ee398b59be1c899a2892c3d20a8f2ee0c648b
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-29 19:04:04 -0400

    Update dateCalc.js

--- dateCalc.js
+++ dateCalc.js
@@ -4,51 +4,55 @@ function dateCalc() {
     newContainer.style.display = 'flex';
     newContainer.style.flexDirection = 'column';
     newContainer.innerHTML = `
-        <div class="commonPopup-header">
-            <button class="commonback-button" id="commonback-button">Back</button>
-            Date Calculator
-        </div>
-        <div class="sections-container">
-            <div class="section-side">
-                <div class="section" id="section-1"></div>
-                <div class="section" id="section-2"></div>
-                <div class="section" id="section-3"></div>
-            </div>
+        <div class="commonPopup-header">Date Calculator</div>
+        <div class="securityPopup-row">
+            <div class="commonSection" id="section1">Section 1</div>
             <div class="vertical-divider"></div>
-            <div class="section-side">
-                <div class="section" id="section-4"></div>
-                <div class="section" id="section-5"></div>
-                <div class="section" id="section-6"></div>
-            </div>
+            <div class="commonSection" id="section2">Section 2</div>
+        </div>
+        <div class="securityPopup-row">
+            <div class="commonSection" id="section3">Section 3</div>
+            <div class="vertical-divider"></div>
+            <div class="commonSection" id="section4">Section 4</div>
+        </div>
+        <div class="securityPopup-row">
+            <div class="commonSection" id="section5">Section 5</div>
+            <div class="vertical-divider"></div>
+            <div class="commonSection" id="section6">Section 6</div>
+        </div>
+        <div class="submit-button-container">
+            <button id="submitButton">Submit</button>
         </div>
         <style>
-            .sections-container {
+            .securityPopup-row {
                 display: flex;
                 flex-grow: 1;
-                align-items: center; /* Align children to center */
+                align-items: center;
             }
-            .section-side {
-                flex: 1;
-                display: flex;
-                flex-direction: column;
-            }
-            .section {
+            
+            .commonSection {
                 flex: 1;
                 padding: 20px;
-                margin: 5px;
             }
+
             .vertical-divider {
-                width: 4px;           
-                background-color: #000;
-                margin: 15% 5px;     /* Margin to shorten the divider's length */
-                border-radius: 2px;  /* Slightly rounded corners for aesthetics */
+                width: 5px; /* Make the divider a bit thicker */
+                background-color: #E0E0E0; /* A lighter color for the divider */
+                margin: 15% 2%; /* Shorten its length and add some space on the sides */
+                border-radius: 2.5px;
+            }
+
+            .submit-button-container {
+                padding: 20px;
+                display: flex;
+                justify-content: center;
             }
         </style>
     `;
     document.body.appendChild(newContainer);
     document.getElementById('commonback-button').addEventListener('click', function() {
-	    newContainer.remove();
-	    openPopup();  
-	});
+        newContainer.remove();
+        openPopup();  
+    });
     makePopupMovable(newContainer);
 }
