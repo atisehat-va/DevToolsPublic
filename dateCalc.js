62a6360bcdac759c8e58204244ed2b57a8d0181b
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-29 18:16:44 -0400

    Update dateCalc.js

--- dateCalc.js
+++ dateCalc.js
@@ -7,19 +7,19 @@ function dateCalc() {
             Date Calculator
         </div>        
         <div class="grid-container">
-            <div class="grid-item">Content 1</div>
-            <div class="grid-item">Content 2</div>
-            <div class="grid-item">Content 3</div>
-            <div class="grid-item">Content 4</div>
-            <div class="grid-item">Content 5</div>
-            <div class="grid-item">Content 6</div>
+            <div class="grid-item" id="section-1">1</div>
+            <div class="grid-item" id="section-2">2</div>
+            <div class="grid-item" id="section-3">3</div>
+            <div class="grid-item" id="section-4">4</div>
+            <div class="grid-item" id="section-5">5</div>
+            <div class="grid-item" id="section-6">6</div>
         </div>
         <style>
             .grid-container {
                 display: grid;
-                grid-template-columns: repeat(2, 1fr); /* creates 2 equal columns */
-                grid-template-rows: repeat(3, 1fr);   /* creates 3 equal rows */
-                gap: 10px; /* gap between grid items */
+                grid-template-columns: repeat(2, 1fr);
+                grid-template-rows: repeat(3, 1fr);
+                gap: 10px;
                 width: 100%;
                 border: 1px solid black;
             }
