03095bfd2f726bca017152b96a4edab4f0ed23c9
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-29 18:14:01 -0400

    Update dateCalc.js

--- dateCalc.js
+++ dateCalc.js
@@ -6,9 +6,32 @@ function dateCalc() {
             <button class="commonback-button" id="commonback-button">Back</button>
             Date Calculator
         </div>        
-        <div class="dirtyFieldsPopup-content">
-            <!-- Placeholder for content to be added later -->
+        <div class="grid-container">
+            <div class="grid-item">Content 1</div>
+            <div class="grid-item">Content 2</div>
+            <div class="grid-item">Content 3</div>
+            <div class="grid-item">Content 4</div>
+            <div class="grid-item">Content 5</div>
+            <div class="grid-item">Content 6</div>
         </div>
+        <style>
+            .grid-container {
+                display: grid;
+                grid-template-columns: repeat(2, 1fr); /* creates 2 equal columns */
+                grid-template-rows: repeat(3, 1fr);   /* creates 3 equal rows */
+                gap: 10px; /* gap between grid items */
+                width: 100%;
+                border: 1px solid black;
+            }
+
+            .grid-item {
+                border: 1px solid black;
+                padding: 10px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+        </style>
     `;
     document.body.appendChild(newContainer);
     document.getElementById('commonback-button').addEventListener('click', function() {
