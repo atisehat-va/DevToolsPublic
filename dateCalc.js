2644846789cd70ddcbd1a3da4e0e11385ebce031
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-29 17:59:54 -0400

    Update dateCalc.js

--- dateCalc.js
+++ dateCalc.js
@@ -1,31 +1,13 @@
-function generateDirtyFieldsHtml(dirtyFields) {
-    if (dirtyFields.length > 0) {
-        return dirtyFields.map((attribute, index) => {
-            const logicalName = attribute.getName();
-            const control = attribute.controls.get(0);
-            const displayName = control ? control.getLabel() : logicalName;
-            return `
-                <div>${index + 1}. <strong>${displayName}</strong>
-                    <div style="margin-left: 40px;">&bull; <strong>Logical Name:</strong> ${logicalName}</div>
-                </div>
-                <br>
-            `;
-        }).join('');
-    } else {
-        return '<div>No dirty fields found.</div>';
-    }
-}
-
-function appendDirtyFieldsPopupToBody(html) {
+function dateCalc() {
     var newContainer = document.createElement('div');
     newContainer.className = 'commonPopup';
     newContainer.innerHTML = `
         <div class="commonPopup-header">
             <button class="commonback-button" id="commonback-button">Back</button>
-            Date Calculator
+            Dirty Fields Info
         </div>        
         <div class="dirtyFieldsPopup-content">
-            ${html}
+            <!-- Placeholder for content to be added later -->
         </div>
     `;
     document.body.appendChild(newContainer);
@@ -34,19 +16,4 @@ function appendDirtyFieldsPopupToBody(html) {
 	    openPopup();  
 	});
     makePopupMovable(newContainer);
-}
-
-function dateCalc() {       
-    const entity = Xrm.Page.data.entity;
-    const attributes = entity.attributes.get();
-    const dirtyFields = attributes.filter(attribute => attribute.getIsDirty());
-    const dirtyFieldsHtml = generateDirtyFieldsHtml(dirtyFields);
-    const popupHtml = `
-        <h2 style="text-align: left;"><strong>Dirty Fields:</strong></h2>
-        <div class="scrollable-section" style="padding: 10px; columns: 2; -webkit-columns: 2; -moz-columns: 2;">
-            ${dirtyFieldsHtml}
-        </div>
-    `;
-
-    appendDirtyFieldsPopupToBody(popupHtml);
 }
