da8bc0069f17afd8b387c2f93677646fc14aedb7
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-09-29 12:00:25 -0400

    Update userProvisionOLD.js

--- userProvisionOLD.js
+++ userProvisionOLD.js
@@ -45,3 +45,111 @@ function openUrl(pageType) {
     appendUserProvisionPopupToBody(popupHtml, formUrl); 
     }      
 }
+
+//test
+function calculateAdjustedDate() {
+debugger;    
+    function stripTimeFromDate(date) {
+        return new Date(date.setHours(0,0,0,0));
+    }
+
+    function getCaseDate(caseId) {
+        return Xrm.WebApi.online.retrieveRecord("mcs_vethomecase", caseId, "?$select=mcs_closuredate");
+    }
+
+    function getHolidaysForSchedule() {
+        return new Promise((resolve, reject) => {
+            const fetchXml = `
+                <fetch>
+                    <entity name="calendar">
+                        <filter>
+                            <condition attribute="name" operator="eq" value="Federal Holiday Schedule" />
+                        </filter>
+                        <link-entity name="calendarrule" from="calendarid" to="calendarid" alias="rule">
+                            <attribute name="name" />
+                            <attribute name="starttime" />
+                            <filter>
+                                <condition attribute="starttime" operator="this-year" />
+                            </filter>
+                        </link-entity>
+                    </entity>
+                </fetch>
+            `;
+            Xrm.WebApi.retrieveMultipleRecords("calendar", `?fetchXml=${encodeURIComponent(fetchXml)}`).then(
+                results => {
+                    let holidays = results.entities.map(entity => stripTimeFromDate(new Date(entity["rule.starttime"])));
+                    resolve(holidays);
+                },
+                error => {
+                    reject(error.message);
+                }
+            );
+        });
+    }
+    
+    function isWeekend(date) {
+        return date.getUTCDay() === 0 || date.getUTCDay() === 6;
+    }
+    
+    var parentCase = CommCare.Shared.GetFieldValue("mcs_parentvethomecaseid");
+    var parentCaseId = CommCare.Shared.GetCleanId(parentCase);    
+    
+    getCaseDate(parentCaseId)
+    .then(function(caseResult) {
+        var caseDate = stripTimeFromDate(new Date(caseResult.mcs_closuredate));          
+        var reopenReason = CommCare.Shared.GetFieldValue("mcs_reopenreasonid");
+        var reopenReasonId = CommCare.Shared.GetCleanId(reopenReason);        
+
+        return Xrm.WebApi.online.retrieveRecord("vhacrm_actionintersection", reopenReasonId, "?$select=mcs_vethomesla")       
+        .then(function (slaResults) {
+            var slaNumber = slaResults.mcs_vethomesla;
+            return { slaNumber: slaNumber, caseDate: caseDate };
+        });
+    })
+    .then(function(data) {
+        return getHolidaysForSchedule()
+        .then(function(holidays) {
+            return { holidays: holidays, slaNumber: data.slaNumber, caseDate: data.caseDate };
+        });
+    })
+    .then(function(data) {
+        var addedDays = 0;
+        var closureDate = data.caseDate;
+        var slaNumber = data.slaNumber;
+        var holidayStrings = data.holidays.map(h => h.toISOString().split("T")[0]);
+
+        while (addedDays < slaNumber) {
+            closureDate.setDate(closureDate.getDate() + 1);
+            closureDate = stripTimeFromDate(closureDate); 
+
+            if (isWeekend(closureDate) || holidayStrings.includes(closureDate.toISOString().split("T")[0])) {
+                continue;
+            }
+            addedDays++;
+        }
+        var adjustedDate = closureDate;
+        console.log("Adjusted Date:", adjustedDate);
+        var today = stripTimeFromDate(new Date());
+        
+        if (adjustedDate < today) {
+           const alertStrings = {
+                text: "Unable to create a Case Addendum: The associated Case exceeds the permitted timeframe for the selected Reopen Reason.",
+                title: "Validation Error"
+            };
+
+            const alertOptions = { height: 200, width: 300 };
+            Xrm.Navigation.openAlertDialog(alertStrings, alertOptions)
+            .then(success => {
+                CommCare.Shared.SetFieldValue("mcs_reopenreasonid", null); 
+            })
+            .catch(error => {
+                console.log("Error in closing dialog", error);
+            });                       
+        }
+    })
+    .catch(function(error) {
+        console.error(error.message);
+    });
+}
+
+
