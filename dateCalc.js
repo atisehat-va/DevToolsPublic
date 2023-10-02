81fcdfa945ee118cb1f6e6a0bd38d95ab137cd8c
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-10-02 12:58:16 -0400

    Update dateCalc.js

--- dateCalc.js
+++ dateCalc.js
@@ -1,49 +1,43 @@
-function dateCalc() {
-    var newContainer = document.createElement('div');
-    newContainer.className = 'commonPopup';
-    newContainer.style.display = 'flex';
-    newContainer.style.flexDirection = 'column';
-    newContainer.innerHTML = `
-         <div class="commonPopup-header">Copy User Security</div>
-	  <button class="commonback-button" id="commonback-button">Back</button>
-   
-	  <div class="securityPopup-row">
-	    <div class="calcDate-section-row1" id="section1">
-	      <h3>Calendar Info</h3>
-	      <ul id="holidaysList"></ul>     			      
-	    </div>     
-	  </div> 
-      	
-	  <div class="securityPopup-row">
-	    <div class="commonSection details-section-row" id="section2">
-	      <h3>Calendar 1</h3>			      			      
-	    </div>       
-	    <div class="commonSection details-section-row" id="section3">
-	      <h3>Calendar 2</h3>			      			      
-	    </div>
-	  </div>  	  
+async function fetchAllHolidaySchedules() {
+    const fetchXml = `
+        <fetch distinct="true">
+            <entity name="calendar">
+                <attribute name="name" />
+            </entity>
+        </fetch>
+    `;
 
-	  <div class="submit-button-container">
-	    <button id="submitButton">Submit</button>
-	  </div>
-    `;	
-    document.body.appendChild(newContainer);
-    document.getElementById('commonback-button').addEventListener('click', function() {
-        newContainer.remove();
-        openPopup();  
-    });
-    makePopupMovable(newContainer);
-
-    // Fetch and display holidays
-    displayHolidays();
+    const results = await Xrm.WebApi.retrieveMultipleRecords("calendar", `?fetchXml=${encodeURIComponent(fetchXml)}`);
+    return results.entities.map(entity => entity.name);
 }
 
-async function getHolidaysForSchedule() {
+async function setupHolidayScheduleDropdown() {
+    const schedules = await fetchAllHolidaySchedules();
+
+    const dropdown = document.createElement('select');
+    dropdown.id = 'holidayScheduleDropdown';
+
+    schedules.forEach(schedule => {
+        const option = document.createElement('option');
+        option.value = schedule;
+        option.innerText = schedule;
+        dropdown.appendChild(option);
+    });
+
+    const container = document.querySelector('.commonPopup-header');
+    container.appendChild(dropdown);
+
+    dropdown.addEventListener('change', (e) => {
+        displayHolidays(e.target.value);
+    });
+}
+
+async function getHolidaysForSchedule(scheduleName = 'Federal Holiday Schedule') {
     const fetchXml = `
         <fetch>
             <entity name="calendar">
                 <filter>
-                    <condition attribute="name" operator="eq" value="Federal Holiday Schedule" />
+                    <condition attribute="name" operator="eq" value="${scheduleName}" />
                 </filter>
                 <link-entity name="calendarrule" from="calendarid" to="calendarid" alias="rule">
                     <attribute name="name" />
@@ -63,9 +57,9 @@ async function getHolidaysForSchedule() {
     }));
 }
 
-async function displayHolidays() {
+async function displayHolidays(scheduleName) {
     try {
-        const holidays = await getHolidaysForSchedule();
+        const holidays = await getHolidaysForSchedule(scheduleName);
 
         // Sort holidays by date
         holidays.sort((a, b) => new Date(a.date) - new Date(b.date));
@@ -79,38 +73,78 @@ async function displayHolidays() {
             const formattedDate = `${fullDayName} - ${("0" + (dateObject.getMonth() + 1)).slice(-2)}/${("0" + dateObject.getDate()).slice(-2)}/${dateObject.getFullYear()}`;
             return `<div class="holidayRow"><div class="holidayName"><b>${holiday.name}</b></div><div class="holidayDate">${formattedDate}</div></div>`;
         }).join('');
-
-        // CSS (remains unchanged)
-        const styles = `
-            #holidaysList {
-                max-height: 85%;
-                overflow-y: auto;
-                display: grid;
-                margin-top: 15px;
-                margin-left: 10px;
-            }
-            .holidayRow {
-                display: grid;
-                grid-template-columns: 1fr 1fr;
-                align-items: center;
-            }
-            .holidayName, .holidayDate {
-                padding: 4px 8px;
-                border: 1px solid #ddd;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                text-align: left;
-            }
-        `;
-
-        // Append styles to the document
-        const styleSheet = document.createElement("style");
-        styleSheet.type = "text/css";
-        styleSheet.innerText = styles;
-        document.head.appendChild(styleSheet);
-
     } catch (error) {
         console.error("Error fetching holidays: ", error);
     }
 }
+
+function dateCalc() {
+    var newContainer = document.createElement('div');
+    newContainer.className = 'commonPopup';
+    newContainer.style.display = 'flex';
+    newContainer.style.flexDirection = 'column';
+    newContainer.innerHTML = `
+         <div class="commonPopup-header">Copy User Security</div>
+         <button class="commonback-button" id="commonback-button">Back</button>
+   
+         <div class="securityPopup-row">
+             <div class="calcDate-section-row1" id="section1">
+                 <h3>Calendar Info</h3>
+                 <div id="holidaysList"></div>     			      
+             </div>     
+         </div> 
+      	
+         <div class="securityPopup-row">
+             <div class="commonSection details-section-row" id="section2">
+                 <h3>Calendar 1</h3>			      			      
+             </div>       
+             <div class="commonSection details-section-row" id="section3">
+                 <h3>Calendar 2</h3>			      			      
+             </div>
+         </div>  	  
+
+         <div class="submit-button-container">
+             <button id="submitButton">Submit</button>
+         </div>
+    `;	
+    document.body.appendChild(newContainer);
+    document.getElementById('commonback-button').addEventListener('click', function() {
+        newContainer.remove();
+        openPopup();  
+    });
+    makePopupMovable(newContainer); 
+
+    // Fetch and display holidays
+    setupHolidayScheduleDropdown();
+    displayHolidays('Federal Holiday Schedule'); 
+}
+
+// Add CSS styles for the grid layout (you can append this to your existing styles)
+const styles = `
+    #holidaysList {
+        max-height: 85%;
+        overflow-y: auto;
+        display: grid;
+        margin-top: 15px;
+        margin-left: 10px;
+    }
+    .holidayRow {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        align-items: center;
+    }
+    .holidayName, .holidayDate {
+        padding: 4px 8px;
+        border: 1px solid #ddd;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        text-align: left;
+    }
+`;
+
+// Append styles to the document
+const styleSheet = document.createElement("style");
+styleSheet.type = "text/css";
+styleSheet.innerText = styles;
+document.head.appendChild(styleSheet);
