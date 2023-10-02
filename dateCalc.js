30f5e624432c5c40c3808b78ddbbf48762ad5656
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-10-02 12:09:35 -0400

    Update dateCalc.js

--- dateCalc.js
+++ dateCalc.js
@@ -9,7 +9,8 @@ function dateCalc() {
    
 	  <div class="securityPopup-row">
 	    <div class="calcDate-section-row1" id="section1">
-	      <h3>Calendar Info</h3>			      			      
+	      <h3>Calendar Info</h3>
+	      <ul id="holidaysList"></ul>     			      
 	    </div>     
 	  </div> 
       	
@@ -32,4 +33,54 @@ function dateCalc() {
         openPopup();  
     });
     makePopupMovable(newContainer);
+
+    // Fetch and display holidays
+    displayHolidays();
+}
+
+function getHolidaysForSchedule() {
+    return new Promise((resolve, reject) => {
+        const fetchXml = `
+            <fetch>
+                <entity name="calendar">
+                    <filter>
+                        <condition attribute="name" operator="eq" value="Federal Holiday Schedule" />
+                    </filter>
+                    <link-entity name="calendarrule" from="calendarid" to="calendarid" alias="rule">
+                        <attribute name="name" />
+                        <attribute name="starttime" />
+                        <filter>
+                            <condition attribute="starttime" operator="this-year" />
+                        </filter>
+                    </link-entity>
+                </entity>
+            </fetch>
+        `;
+        Xrm.WebApi.retrieveMultipleRecords("calendar", `?fetchXml=${encodeURIComponent(fetchXml)}`).then(
+            results => {
+                let holidays = results.entities.map(entity => stripTimeFromDate(new Date(entity["rule.starttime"])));
+                resolve(holidays);
+            },
+            error => {
+                reject(error.message);
+            }
+        );
+    });
+}
+
+function stripTimeFromDate(date) {
+    return new Date(date.setHours(0, 0, 0, 0));
+}
+
+function displayHolidays() {
+    getHolidaysForSchedule().then(holidays => {
+        const holidaysList = document.getElementById('holidaysList');
+        holidays.forEach(holiday => {
+            const listItem = document.createElement('li');
+            listItem.textContent = holiday.toDateString();
+            holidaysList.appendChild(listItem);
+        });
+    }).catch(error => {
+        console.error("Error fetching holidays: ", error);
+    });
 }
