f387c853eac6248f2734ba09a8f6a56f8b193c8e
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-10-03 15:52:59 -0400

    Update dateCalc.js

--- dateCalc.js
+++ dateCalc.js
@@ -270,7 +270,7 @@ const calendarStyles = `
         color: white;
     }
     #calendarDates .holidayDate {
-        color: #2196F3;
+        color: #056d05;
     }
     .todayDate {         
         background-color: #056d05 !important;
