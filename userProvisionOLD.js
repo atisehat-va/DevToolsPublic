81eb35b66f87555acbdf048e03d3c9089c331b61
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-10-09 15:12:26 -0400

    Update userProvisionOLD.js

--- userProvisionOLD.js
+++ userProvisionOLD.js
@@ -75,3 +75,14 @@ function LaunchAddendum(primaryControl) {
         function (error) { console.log("Failed to open addendum (case)"); }
     );  
 }
+
+var alertStrings = { confirmButtonLabel: "Yes", text: "This is an alert.", title: "Sample title" };
+var alertOptions = { height: 120, width: 260 };
+Xrm.Navigation.openAlertDialog(alertStrings, alertOptions).then(
+    function (success) {
+        console.log("Alert dialog closed");
+    },
+    function (error) {
+        console.log(error.message);
+    }
+);
