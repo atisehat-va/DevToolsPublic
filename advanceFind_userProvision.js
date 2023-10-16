db16abba2f0e9892db3e49bfbca1b01d7d231ad1
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-10-16 13:51:11 -0400

    Update advanceFind_userProvision.js

--- advanceFind_userProvision.js
+++ advanceFind_userProvision.js
@@ -35,9 +35,26 @@ function openUrl(pageType) {
         const windowOptions = "height=650,width=950,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,titlebar=no,toolbar=no";
         window.open(advancedFindUrl, windowName, windowOptions);        
     } else if (pageType === "userProvision") {
-        const entityName = "vhacrm_userprovision";
-        const formUrl = clientUrl + "/main.aspx?etn=" + entityName + "&pagetype=entityrecord";    
-        const popupHtml = ` `;
-        appendUserProvisionPopupToBody(popupHtml, formUrl); 
-    }      
+        const entityName = "vhacrm_userprovision1";
+        const formUrl = clientUrl + "/main.aspx?etn=" + entityName + "&pagetype=entityrecord";
+        
+        // Check if the entity exists
+        const req = new XMLHttpRequest();
+        req.open("GET", `${clientUrl}/api/data/v9.0/EntityDefinitions(LogicalName='${entityName}')`, true);
+        req.setRequestHeader("OData-MaxVersion", "4.0");
+        req.setRequestHeader("OData-Version", "4.0");
+        req.setRequestHeader("Accept", "application/json");
+        req.onreadystatechange = function () {
+          if (this.readyState === 4) {
+            if (this.status === 200) {
+              const popupHtml = ` `;
+              appendUserProvisionPopupToBody(popupHtml, formUrl);
+            } else if (this.status === 404) {
+              showCustomAlert(`This tool isn't available.`);
+            }
+          }
+        };
+        req.send();
+      }
+    }
 }
