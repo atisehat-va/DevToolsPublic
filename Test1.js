6ec07945f1103256d5eb06242c625cdfc8f6638f
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-10-16 14:40:44 -0400

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -51,74 +51,70 @@ function openPopup() {
   if (!isAdmin && userName !== "Adrian Solis") {
     Xrm.Navigation.openAlertDialog({ text: "You do not have permission to execute this action."});
     return;    
-  }
-
-
-
-	 
-	  var popupHtml = `  
-	    <style>       
-	        .popup { position: fixed; left: 50%; top: 50%; background-color: #f9f9f9; border: 1px solid #888; border-radius: 10px; box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.2); transform: translate(-50%, -50%); max-height: 80vh; overflow-y: auto; width: 420px; }	 	
-		.button-container { padding: 20px; width: 380px; }
-  		.popup button { display: block; width: 100%; margin-bottom: 10px; padding: 10px; background-color: #102e55; color: white; border: none; border-radius: 20px; cursor: pointer; transition: all 0.3s ease; box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2); }
-    		.popup button:hover { background-color: #3c6690; transform: translateY(-2px); box-shadow: 0 7px 20px rgba(0, 0, 0, 0.25); }
-      		.popup button:active { transform: translateY(0); box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2); }		
-		.button-row { display: flex; justify-content: space-between; flex-direction: row; width: 100%; }
-		.button-row button { width: calc(50% - 5px); }
-		.dropdown button { width: 100%; }
-		.button-row .full-width { width: 100%; }
-		.dropdown-row { display: flex; justify-content: space-between; flex-direction: row; width: 100%; }
-		.dropdown { position: relative; display: inline-block; width: calc(50% - 5px); }
-		.dropdown-content { display: none; position: absolute; min-width: 100%; z-index: 1; }
-		.dropdown-content button { display: block; background-color: white; color: black; padding: 10px; text-align: center; border: none; width: 100%; }
-  		.popup button.close-btn { margin-top: 10px; font-size: 15px; }
-	    </style>
-	    <div class="popup">
-     		<div class="commonPopup-header">	            
-	            Admin Plus
-	        </div>
-	    	<div class="button-container">
-      		  <div class="button-row">
-		    <button onclick="closePopup(); openUrl('advanceFind');">Advanced Find Classic</button>
-		    <button onclick="closePopup(); setTimeout(fetchEntityFields, 0);">Show Entity Info</button>
-		  </div>	  		  
-		  <div class="button-row">
-		    <button onclick="showAllTabsAndSections();">Show Hidden Items</button>
-		    <button onclick="renameTabsSectionsFields();">Show Logical Names</button>
-		  </div>
-		  <div class="button-row">		    
-		    <button onclick="unlockAllFields();">Unlock All Fields</button>
-      		    <button onclick="closePopup(); showDirtyFields();">Show Modified Fields</button>
-		  </div>		  	  
-    		  <div class="button-row">
-		    <button onclick="closePopup(); securityUpdate2();">Assign User Security</button>
-		    <button onclick="closePopup(); securityUpdate();">Copy User Security</button>
-		  </div>    		  
-    		  <div class="button-row">		    
-      		    <button onclick="closePopup(); dateCalc();">Date Calc</button>
-		    <div class="dropdown">
-		      <button onclick="toggleDropdownMenu('dropdown-content');">Extra</button>
-		      <div id="dropdown-content" class="dropdown-content">			 
-		         <button onclick="openRestBuilder(getOrgUrl());">Open REST Builder</button>
-	   		 <button onclick="closePopup(); openUrl(\'userProvision\');">User Provision Tool</button>		
-		      </div>
-		    </div>
-		  </div>
-		    <button onclick="closePopup();" class="close-btn">Close</button>
-		</div>
-	        <div id="popupContent" class="content"></div>	
-	   </div>
-	  `;	  
-	  var newContainer = document.createElement('div');
-	  newContainer.id = 'MenuPopup';
-	  newContainer.innerHTML = popupHtml;
-	  newContainer.style.position = 'fixed';  
-	  newContainer.style.left = '50%';
-	  newContainer.style.top = '50%';
-	  newContainer.style.transform = 'translate(-50%, -50%)';  
-	  document.body.appendChild(newContainer);
-	  
-	  makePopupMovable(newContainer);
+  }	 
+  var popupHtml = `  
+    <style>       
+	.popup { position: fixed; left: 50%; top: 50%; background-color: #f9f9f9; border: 1px solid #888; border-radius: 10px; box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.2); transform: translate(-50%, -50%); max-height: 80vh; overflow-y: auto; width: 420px; }	 	
+	.button-container { padding: 20px; width: 380px; }
+	.popup button { display: block; width: 100%; margin-bottom: 10px; padding: 10px; background-color: #102e55; color: white; border: none; border-radius: 20px; cursor: pointer; transition: all 0.3s ease; box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2); }
+	.popup button:hover { background-color: #3c6690; transform: translateY(-2px); box-shadow: 0 7px 20px rgba(0, 0, 0, 0.25); }
+	.popup button:active { transform: translateY(0); box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2); }		
+	.button-row { display: flex; justify-content: space-between; flex-direction: row; width: 100%; }
+	.button-row button { width: calc(50% - 5px); }
+	.dropdown button { width: 100%; }
+	.button-row .full-width { width: 100%; }
+	.dropdown-row { display: flex; justify-content: space-between; flex-direction: row; width: 100%; }
+	.dropdown { position: relative; display: inline-block; width: calc(50% - 5px); }
+	.dropdown-content { display: none; position: absolute; min-width: 100%; z-index: 1; }
+	.dropdown-content button { display: block; background-color: white; color: black; padding: 10px; text-align: center; border: none; width: 100%; }
+	.popup button.close-btn { margin-top: 10px; font-size: 15px; }
+    </style>
+    <div class="popup">
+	<div class="commonPopup-header">	            
+	    Admin Plus
+	</div>
+	<div class="button-container">
+	  <div class="button-row">
+	    <button onclick="closePopup(); openUrl('advanceFind');">Advanced Find Classic</button>
+	    <button onclick="closePopup(); setTimeout(fetchEntityFields, 0);">Show Entity Info</button>
+	  </div>	  		  
+	  <div class="button-row">
+	    <button onclick="showAllTabsAndSections();">Show Hidden Items</button>
+	    <button onclick="renameTabsSectionsFields();">Show Logical Names</button>
+	  </div>
+	  <div class="button-row">		    
+	    <button onclick="unlockAllFields();">Unlock All Fields</button>
+	    <button onclick="closePopup(); showDirtyFields();">Show Modified Fields</button>
+	  </div>		  	  
+	  <div class="button-row">
+	    <button onclick="closePopup(); securityUpdate2();">Assign User Security</button>
+	    <button onclick="closePopup(); securityUpdate();">Copy User Security</button>
+	  </div>    		  
+	  <div class="button-row">		    
+	    <button onclick="closePopup(); dateCalc();">Date Calc</button>
+	    <div class="dropdown">
+	      <button onclick="toggleDropdownMenu('dropdown-content');">Extra</button>
+	      <div id="dropdown-content" class="dropdown-content">			 
+		 <button onclick="openRestBuilder();">Open REST Builder</button>
+		 <button onclick="closePopup(); openUrl('userProvision');">User Provision Tool</button>		
+	      </div>
+	    </div>
+	  </div>
+	    <button onclick="closePopup();" class="close-btn">Close</button>
+	</div>
+	<div id="popupContent" class="content"></div>	
+   </div>
+  `;	  
+  var newContainer = document.createElement('div');
+  newContainer.id = 'MenuPopup';
+  newContainer.innerHTML = popupHtml;
+  newContainer.style.position = 'fixed';  
+  newContainer.style.left = '50%';
+  newContainer.style.top = '50%';
+  newContainer.style.transform = 'translate(-50%, -50%)';  
+  document.body.appendChild(newContainer);
+  
+  makePopupMovable(newContainer);
 	
 }
 function clearCacheFunction() {
