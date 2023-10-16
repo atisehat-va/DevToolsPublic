aafb5dd380c33fbea7b414379c55e182d94f6fb6
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-10-16 14:13:35 -0400

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -100,7 +100,7 @@ function openPopup() {
 		      <button onclick="toggleDropdownMenu('dropdown-content');">Extra</button>
 		      <div id="dropdown-content" class="dropdown-content">			 
 		         <button onclick="openRestBuilder(getOrgUrl());">Open REST Builder</button>
-	   		 <button onclick="closePopup(); openUrl('userProvision');">User Provision Tool</button>			
+	   		 <button onclick="closePopup(); openUrl(\'userProvision\');">User Provision Tool</button>		
 		      </div>
 		    </div>
 		  </div>
