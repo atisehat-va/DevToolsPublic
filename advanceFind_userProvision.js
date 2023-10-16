a5772f7610b5c797f24069457b22838352ead6ee
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-10-16 15:07:45 -0400

    Update advanceFind_userProvision.js

--- advanceFind_userProvision.js
+++ advanceFind_userProvision.js
@@ -57,34 +57,3 @@ function openUrl(pageType) {
         req.send();
   }
 }
-
-//RestBuilder
-function openRestBuilder(orgUrl) {
-  //closeIframe();
-  var restBuilderPath = '/WebResources/lat_/CRMRESTBuilder/Xrm.RESTBuilder.htm#';
-  var restBuilderUrl = orgUrl + restBuilderPath;
-  var windowOptions = "height=600,width=800,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,titlebar=no,toolbar=no";
-  
-  // Check if the URL exists
-  var req = new XMLHttpRequest();
-  req.open('HEAD', restBuilderUrl, true);
-  req.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      if (this.status === 200) {
-        window.open(restBuilderUrl, "REST Builder", windowOptions);
-      } else {        
-        showCustomAlert(`This tool isn't available.`);
-      }
-    }
-  };
-  req.send();
-}
-
-function getOrgUrl() {
-  if (typeof Xrm !== 'undefined' && Xrm.Page && Xrm.Page.context) {
-    return Xrm.Page.context.getClientUrl();
-  } else {
-    console.error('Unable to retrieve organization URL. Pleaes run this within a Dynamics CRM environment.');
-    return '';
-  }
-}
