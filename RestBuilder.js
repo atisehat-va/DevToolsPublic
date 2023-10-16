ecccafcf7867eab0a676d152659a4213ac649a83
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-10-16 12:54:12 -0400

    Update RestBuilder.js

--- RestBuilder.js
+++ RestBuilder.js
@@ -3,8 +3,22 @@ function openRestBuilder(orgUrl) {
   var restBuilderPath = '/WebResources/lat_/CRMRESTBuilder/Xrm.RESTBuilder.htm#';
   var restBuilderUrl = orgUrl + restBuilderPath;
   var windowOptions = "height=600,width=800,location=no,menubar=no,resizable=yes,scrollbars=yes,status=no,titlebar=no,toolbar=no";
-  window.open(restBuilderUrl, "REST Builder", windowOptions);
+  
+  // Check if the URL exists
+  var req = new XMLHttpRequest();
+  req.open('HEAD', restBuilderUrl, true);
+  req.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        window.open(restBuilderUrl, "REST Builder", windowOptions);
+      } else {
+        Xrm.Navigation.openAlertDialog({ text: "This tool isn't available." });
+      }
+    }
+  };
+  req.send();
 }
+
 function getOrgUrl() {
   if (typeof Xrm !== 'undefined' && Xrm.Page && Xrm.Page.context) {
     return Xrm.Page.context.getClientUrl();
