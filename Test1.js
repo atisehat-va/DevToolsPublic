2832ece1ce681bfb8a5cf916316a0824be120ce7
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-10-16 10:25:04 -0400

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -84,8 +84,8 @@ function openPopup() {
                       <button onclick="closePopup(); openUrl('advanceFind');">Advanced Find Classic</button>		      
 		    </div>
 		    <div class="dropdown">
-		      <button onclick="toggleDropdownMenu('dropdown-content');">Update Security</button>
-		      <div id="dropdown-content" class="dropdown-content">		        
+		      <button onclick="toggleDropdownMenu('dropdown-content1');">Update Security</button>
+		      <div id="dropdown-content1" class="dropdown-content">		        
 			${userProvisionButton}
 		         <button onclick="closePopup(); securityUpdate();">Copy User Security</button>
 		    	 <button onclick="closePopup(); securityUpdate2();">Assign User Security</button>	                		        
