03f02229cd0572c14ba6fd9995d4c235cd50ad10
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-11-08 01:02:55 -0500

    Update editSecurity.js

--- editSecurity.js
+++ editSecurity.js
@@ -498,6 +498,7 @@ function editSecurity() {
 			    await updateUserDetails(selectedUserId, businessUnitRadioSelected, teamsCheckedValues, rolesCheckedValues, "AddTeams");
 			} else {
 			    console.log('No update needed on Teams');
+			    showCustomAlert(`To update user Teams, please select one or more Teams and one of the followign Actions: Add, Remove, Add + Remove Existing.`);
 			}	        
 		    }	
 		    if (rolesRadioSelected && rolesCheckedValues.length > 0) {
@@ -510,6 +511,7 @@ function editSecurity() {
 			    await updateUserDetails(selectedUserId, businessUnitRadioSelected, teamsCheckedValues, rolesCheckedValues, "AddRoles");
 			} else {
 			    console.log('No update needed on Teams');
+			    showCustomAlert(`To update user roles, please select one or more roles and one of the following actions: Add, Remove, or Add + Remove Existing.`);
 			}
 		    }
 		    document.getElementById('section3').querySelector('ul').innerHTML = '';  //Clear BusinessUnit & Teams List
