e34d138fbe1423903719957c8fbbe9326d9d1202
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-11-09 16:49:49 -0500

    Update common.js

--- common.js
+++ common.js
@@ -6,7 +6,7 @@ function fetchRolesForUser(userId, callback) {
 	Xrm.WebApi.retrieveMultipleRecords('systemuserroles', `?$filter=systemuserid eq ${userId}`).then(callback);
 }
 function fetchTeamsForUser(userId, callback) {
-	Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=teammembership_association($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
+	Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=teammembership_association($select=name,teamtype)&$filter=systemuserid eq ${userId}`).then(callback);
 }
 function fetchBusinessUnitName(userId, callback) {
 	Xrm.WebApi.retrieveMultipleRecords('systemuser', `?$select=fullname&$expand=businessunitid($select=name)&$filter=systemuserid eq ${userId}`).then(callback);
