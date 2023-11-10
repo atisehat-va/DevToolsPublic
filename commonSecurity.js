0e26ff4b0637e22324e2ba16fd4e044c09e22d79
Ati Sehat <84085329+atisehat-va@users.noreply.github.com>
2023-11-10 15:31:15 -0500

    Update commonSecurity.js

--- commonSecurity.js
+++ commonSecurity.js
@@ -81,56 +81,14 @@ async function disassociateUserFromRoles(selectedUserId, clientUrl) {
     await fetch(disassociateUrl, { method: "DELETE" });
   }));
 }
-//NewCode
-// Disassociate user from specific roles
 
+// Disassociate user from specific roles
 async function disassociateUserFromSpecificRoles(selectedUserId, selectedRoleIds, clientUrl) {
   await Promise.all(selectedRoleIds.map(async (roleId) => {
     const disassociateUrl = `${clientUrl}/api/data/v9.2/systemusers(${selectedUserId})/systemuserroles_association/$ref?$id=${clientUrl}/api/data/v9.2/roles(${roleId})`;
     await fetch(disassociateUrl, { method: "DELETE" });
   }));
 }
-
-//new 11/10
-/*
-async function disassociateUserFromSpecificRoles(selectedUserId, selectedRoleIds) {
-  if (!selectedUserId || !selectedRoleIds) {
-    console.error("Invalid parameters.");
-    return;
-  }
-
-  // Ensure selectedRoleIds is always an array
-  if (!Array.isArray(selectedRoleIds)) {
-    selectedRoleIds = [selectedRoleIds];
-  }
-
-  try {
-    await Promise.all(selectedRoleIds.map(async (roleId) => {
-      var disassociateRequest = {
-        target: { entityType: "systemuser", id: selectedUserId },
-        relatedEntities: [{ entityType: "role", id: roleId }],
-        relationship: "systemuserroles_association", // Correct relationship name
-        getMetadata: function () {
-          return {
-            boundParameter: null,
-            parameterTypes: {},
-            operationType: 2, // Disassociate operation
-            operationName: "Disassociate"
-          };
-        }
-      };
-
-      const response = await Xrm.WebApi.online.execute(disassociateRequest);
-      if (!response.ok) {
-        console.error(`Failed to disassociate role ${roleId} from user ${selectedUserId}`);
-      }
-    }));
-
-    console.log('Successfully disassociated specified roles from user.');
-  } catch (error) {
-    console.error('Error occurred:', error);
-  }
-} */
 
 // Disassociate user from specific teams
 async function disassociateUserFromSpecificTeams(selectedUserId, selectedTeamIds, clientUrl) {
@@ -139,26 +97,7 @@ async function disassociateUserFromSpecificTeams(selectedUserId, selectedTeamIds
     await fetch(disassociateUrl, { method: "DELETE" });
   }));
 }
-//EndNewCode
 
-/*
-async function disassociateUserFromTeams(selectedUserId, clientUrl) {
-  const teamsUrl = `${clientUrl}/api/data/v9.2/systemusers(${selectedUserId})/teammembership_association`;
-  const response = await fetch(teamsUrl, {
-    headers: { "OData-MaxVersion": "4.0", "OData-Version": "4.0", "Accept": "application/json" }
-  });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error ${response.status}`);
-  }
-
-  const results = await response.json();
-  
-  await Promise.all(results.value.map(async (result) => {
-    const disassociateUrl = `${clientUrl}/api/data/v9.2/teams(${result.teamid})/teammembership_association/$ref?$id=${clientUrl}/api/data/v9.2/systemusers(${selectedUserId})`;
-    await fetch(disassociateUrl, { method: "DELETE" });
-  }));
-} */
 async function disassociateUserFromTeams(selectedUserId, clientUrl) {
   // Assuming that teamtype is a property of the team entity and the API supports the $filter query.
   const teamsUrl = `${clientUrl}/api/data/v9.2/systemusers(${selectedUserId})/teammembership_association?$filter=teamtype eq 0 or teamtype eq 1`;
@@ -193,59 +132,6 @@ async function associateUserToTeam(selectedUserId, selectedTeamIds, clientUrl) {
   });
 } 
 
-/*
-async function associateUserToRole(selectedUserId, selectedRoleIds, clientUrl) {
-  const associateRoleUrl = `${clientUrl}/api/data/v9.2/roles(${selectedRoleIds})/systemuserroles_association/$ref`;
-  const associateRoleData = {
-    "@odata.id": `${clientUrl}/api/data/v9.2/systemusers(${selectedUserId})`
-  };
-  await fetch(associateRoleUrl, {
-    method: "POST",
-    headers: { "OData-MaxVersion": "4.0", "OData-Version": "4.0", "Accept": "application/json", "Content-Type": "application/json; charset=utf-8" },
-    body: JSON.stringify(associateRoleData)
-  });
-}*/
-
-//Test 10/16/2023
-/*
-async function associateUserToRole(selectedUserId, selectedRoleIds, clientUrl) {
-  if (!selectedUserId || !selectedRoleIds || !clientUrl) {
-    console.error("Invalid parameters.");
-    return;
-  }
-
-  const associateRoleUrl = `${clientUrl}/api/data/v9.2/roles(${selectedRoleIds})/systemuserroles_association/$ref?`;
-  const associateRoleData = {
-    "@odata.id": `${clientUrl}/api/data/v9.2/systemusers(${selectedUserId})`
-  };
-  
-  console.log(`Associating role using URL: ${associateRoleUrl}`);
-  console.log(`Associate Role Data: ${JSON.stringify(associateRoleData)}`);
-
-  try {
-    const response = await fetch(associateRoleUrl, {
-      method: "POST",
-      headers: { 
-        "OData-MaxVersion": "4.0", 
-        "OData-Version": "4.0", 
-        "Accept": "application/json", 
-        "Content-Type": "application/json; charset=utf-8"
-      },
-      body: JSON.stringify(associateRoleData)
-    });
-    
-    if (!response.ok) {
-      const responseData = await response.json();
-      console.error('Server returned:', responseData);
-    } else {
-      console.log('Successfully associated role to user.');
-    }
-    
-  } catch (error) {
-    console.error('Fetch failed:', error);
-  }
-} */
-//test11/10
 async function associateUserToRole(selectedUserId, selectedRoleIds) {
   if (!selectedUserId || !selectedRoleIds) {
     console.error("Invalid parameters.");
